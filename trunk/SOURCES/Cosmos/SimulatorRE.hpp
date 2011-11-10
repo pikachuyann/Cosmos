79e95e5c8b537b4a2c1ec57ec2f273596ffee813
barbot2 <barbot2@c602a718-a262-424a-925f-c628338d67b4>
2011-11-10 12:59:42 +0000

    menage
    git-svn-id: https://svn.lsv.ens-cachan.fr/barbot/Cosmos@417 c602a718-a262-424a-925f-c628338d67b4

---
+++ trunk/SOURCES/Cosmos/SimulatorRE.hpp
@@ -0,0 +1,31 @@
+/*
+ *  SimulatorRE.hpp
+ *  Cosmos
+ *
+ *  Created by Benoit Barbot on 09/11/11.
+ *  Copyright 2011 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include "Simulator.hpp"
+
+
+#ifndef _SIMULATOR_RE_HPP
+#define _SIMULATOR_RE_HPP
+
+
+class SimulatorRE: public Simulator{
+public:
+	SimulatorRE(bool);
+	void SimulateSinglePath();
+	void InitialEventsQueue();
+		
+	void GenerateEvent(Event &, int);
+	void GenerateDummyEvent(Event &, int);
+private:
+	bool doubleIS_mode;
+};
+
+
+#endif  /* _SIMULATOR_RE_HPP */
+
