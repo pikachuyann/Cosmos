e19dbae6d8a42874a878dceb6b553cca5873ec1c
barbot2 <barbot2@c602a718-a262-424a-925f-c628338d67b4>
2012-01-13 09:20:12 +0000

    script
    git-svn-id: https://svn.lsv.ens-cachan.fr/barbot/Cosmos@494 c602a718-a262-424a-925f-c628338d67b4

---
+++ trunk/Examples/RareEvent/tandem.ml
@@ -0,0 +1,191 @@
+#!/opt/local/bin/ocaml
+#load "unix.cma";;
+(*#use "../script.ml" *)
+open Printf
+
+let n = int_of_string (Sys.argv.(1));;
+let r = int_of_string (Sys.argv.(2));;
+
+let mu = 0.32;;
+let rho1 = 0.34;;
+let rho2 = 0.34;;
+
+let dir = sprintf "tandem%i_%i" n r;;
+
+
+try 
+  Unix.mkdir dir 0o770;
+with _ -> exit 0;;
+Unix.chdir dir;;
+
+let gspnagr = open_out "tandem_agr.gspn" in
+
+Printf.fprintf gspnagr "
+
+const double mu   = %f;
+const double rho1 = %f;
+const double rho2   = %f;
+const int r = %i;" mu rho1 rho2 r;
+
+Printf.fprintf gspnagr "
+NbPlaces = 3;
+NbTransitions = 3;
+
+PlacesList = { Queue1, Queue2 ,AQueue2 } ;
+TransitionsList = { mu, rho1, rho2};
+
+Marking= {
+(Queue1 ,1);
+(Queue2 ,0);
+(AQueue2,r);
+};
+
+Transitions={
+(mu , EXPONENTIAL(mu),1,1,SINGLE);
+(rho1  , EXPONENTIAL(rho1),1,1,SINGLE);
+(rho2 , EXPONENTIAL(rho2),1,1,SINGLE);
+};
+
+
+InArcs={
+(Queue1    ,rho1,1);
+(AQueue2   ,rho1,1);
+(Queue2   ,rho2,1);
+};
+
+OutArcs={
+(mu ,Queue1   ,1);
+(rho1,Queue2   ,1);
+(rho2,AQueue2  ,1);
+};";
+close_out gspnagr;;
+
+let lhaagr = open_out "tandem_agr.lha";;
+Printf.fprintf lhaagr "
+
+NbVariables = 1;
+NbLocations = 3;
+const int N= %i;
+VariablesList = {x} ;
+
+LocationsList = {l1, lp, lm };AVG(Last(x));
+InitialLocations={l1};
+
+FinalLocations={ lp  };
+
+Locations={
+(l1, (Queue1 + Queue2 > 0) & (Queue1 + Queue2 < N) ) ;
+(lp, (Queue1 + Queue2 = N)  );
+(lm, (Queue1 + Queue2 = 0)  );
+}; 
+
+Edges={
+((l1,l1)  ,ALL,  # , #);
+((l1,lp)  ,ALL,  # , {x=1});
+((l1,lm)  ,ALL,  # , {x=0});
+};" n;
+close_out lhaagr;;
+
+let gspn = open_out "tandem.gspn";;
+Printf.fprintf gspn "
+const double Lambda   = %f;
+const double rho1 = %f;
+const double rho2   = %f;
+const int r = %i;
+" mu rho1 rho2 r;
+
+Printf.fprintf gspn "
+NbPlaces = 7;
+NbTransitions = 8;
+
+PlacesList = { Queue1, Queue2 , RE_Queue1, RE_Queue2 , RE_AntiQueue2 ,ComplQueue2,Puit} ;
+TransitionsList = { LambdaT , rho1T ,rho1Tp,dummyrho1 , rho2T ,rho2Tp,dummyrho2, Puittrans};
+
+Marking= {
+(Queue1 ,1);
+(Queue2 ,0);
+(RE_Queue1   ,1);
+(RE_Queue2   ,0);
+(RE_AntiQueue2   ,r);
+(ComplQueue2 ,0);
+(Puit ,0);
+};
+
+Transitions={
+(LambdaT , EXPONENTIAL(Lambda),1,1,SINGLE);
+(rho1T  , EXPONENTIAL(rho1),1,1,SINGLE);
+(rho2T , EXPONENTIAL(rho2),1,1,SINGLE);
+(rho1Tp  , EXPONENTIAL(rho1),1,1,SINGLE);
+(rho2Tp , EXPONENTIAL(rho2),1,1,SINGLE);
+(dummyrho1  , EXPONENTIAL(rho1),1,1,SINGLE);
+(dummyrho2  , EXPONENTIAL(rho1),1,1,SINGLE);
+(Puittrans, EXPONENTIAL(0*Queue1),1,1,SINGLE);
+};
+
+
+InArcs={
+(Queue1    ,rho1T,1);
+(RE_Queue1 ,rho1T,1);
+(RE_AntiQueue2,rho1T,1);
+
+(Queue1    ,rho1Tp,1);
+
+(Queue2   ,rho2T,1);
+(RE_Queue2,rho2T,1);
+
+(Queue2      ,rho2Tp,1);
+(RE_Queue1   ,rho2Tp,1);
+(ComplQueue2 ,rho2Tp,1);
+};
+
+OutArcs={
+(LambdaT,Queue1   ,1);
+(LambdaT,RE_Queue1,1);
+
+(rho1T,Queue2   ,1);
+(rho1T,RE_Queue2,1);
+
+(rho1Tp,Queue2 ,1);
+(rho1Tp,ComplQueue2,1);
+
+(rho2T,RE_AntiQueue2 ,1);
+
+(Puittrans, Puit,1);
+};
+
+InhibitorArcs={
+(RE_AntiQueue2,rho1Tp,1);
+(RE_AntiQueue2,rho2Tp,1);
+(ComplQueue2,rho2T,1);
+(Queue1,dummyrho1,1);
+(Queue2,dummyrho2,1);
+};";
+close_out gspn;;
+
+let lha = open_out "tandem.lha";;
+Printf.fprintf lha "
+
+NbVariables = 1;
+NbLocations = 3;
+const int N= %i;
+VariablesList = {x} ;
+
+LocationsList = {l1, lp, lm };AVG(Last(x));
+InitialLocations={l1};
+
+FinalLocations={ lp,lm };
+
+Locations={
+(l1, (Queue1 + Queue2 > 0) & (Queue1 + Queue2 < N) & (Puit = 0)) ;
+(lp, (Queue1 + Queue2 = N) & (Puit = 0) );
+(lm, (Queue1 + Queue2 = 0) | (Puit > 0) );
+}; 
+
+Edges={
+((l1,l1)  ,ALL,  # , #);
+((l1,lp)  ,ALL,  # , {x=1});
+((l1,lm)  ,ALL,  # , {x=0});
+};" n;
+close_out lha;;
+
+Sys.command "../../../bin/CosmosGPP tandem_agr.gspn tandem_agr.lha -s &> logcosmos"
