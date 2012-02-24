5391b78aef5be1941f90cec378f1c4e29ce5b437
barbot2 <barbot2@c602a718-a262-424a-925f-c628338d67b4>
2012-02-24 14:06:51 +0000

    tandem prisme
    git-svn-id: https://svn.lsv.ens-cachan.fr/barbot/Cosmos@689 c602a718-a262-424a-925f-c628338d67b4

--- trunk/Examples/RareEventContinuous/tandem.ml
+++ trunk/Examples/RareEventContinuous/tandem.ml
@@ -227,6 +227,12 @@ close_out sm;
   print_endline com1;
   ignore (Sys.command com1);;
 
+let prism1 = "prism tandem_agr.sm ../tandem.csl -fixdl -v -maxiters 100000000 > prismout" in
+  print_endline prism1;
+  ignore (Sys.command prism1);;
+
+  ignore (Sys.command "../traduittable.ml prismout");;
+
 
 
 let exist = try 
