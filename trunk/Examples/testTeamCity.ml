38ee53a40ef2df823b6b7fc4a152942de69038c5
barbot2 <barbot2@c602a718-a262-424a-925f-c628338d67b4>
2012-04-23 11:53:37 +0000

    test caml
    git-svn-id: https://svn.lsv.ens-cachan.fr/barbot/Cosmos@958 c602a718-a262-424a-925f-c628338d67b4

--- trunk/Examples/testTeamCity.ml
+++ trunk/Examples/testTeamCity.ml
@@ -40,7 +40,7 @@ let test_result n v =
   let result = parse_result "Result.res" in
   printf "result: %f\nconfint: [%f,%f]\n" result.mean (fst result.confInt) (snd result.confInt);
   if v > (fst result.confInt) & (snd result.confInt) > v 
-  then printf "##teamcity[testFinished name='%s' message='expected result %f is in confidence interval [%f,%f]']\n" n v (fst result.confInt) (snd result.confInt) 
+  then printf "##teamcity[testFinished name='%s' message='expected result %f is in confidence interval |[%f,%f|]']\n" n v (fst result.confInt) (snd result.confInt) 
   else printf "##teamcity[testFailed name='%s' message='Test %s fail: expected result %f is outside confidence interval |[%f,%f|]']\n" n n v (fst result.confInt) (snd result.confInt) 
 
 let test_cosmos_gspn n v o =
