176eb9e3d02092dc4afc12062d8f796cb79c50e3
barbot2 <barbot2@c602a718-a262-424a-925f-c628338d67b4>
2012-07-12 12:27:33 +0000

    add example for POMDP
    git-svn-id: https://svn.lsv.ens-cachan.fr/barbot/Cosmos@1142 c602a718-a262-424a-925f-c628338d67b4

--- trunk/POMDP/main.cpp
+++ trunk/POMDP/main.cpp
@@ -4,9 +4,9 @@
 #include <fstream>
 #include <iostream>
 #include <string>
-#include <cstdlib>
+//#include <cstdlib>
 #include <sstream>
-#include <unistd.h>
+//#include <unistd.h>
 
 using namespace std;
 
@@ -14,9 +14,20 @@ using namespace std;
 #include "generateSPN.cpp"
 
 int main(int argc, char** argv) {
-    int N =1;
+    int N =5;
     vector< vector<double> > M(N,vector<double>(N,0) );
     vector< vector<double> > Plast(N,vector<double>(N,0) );
+    
+    M[0][1]=1;
+    M[1][1]=0.3;
+    M[1][2]=0.4;
+    M[1][3]=0.3;
+    M[2][0]=0.5;
+    M[2][3]=0.5;
+    M[3][3]=0.3;
+    M[3][4]=0.7;
+    M[4][2]=1;
+    
     
     
     generateLHAfun(N,Plast);
