fd31edfd451cbc1278fa74a56aea2c5ab660bfba
barbot2 <barbot2@c602a718-a262-424a-925f-c628338d67b4>
2012-09-21 09:11:22 +0000

    remove -O3
    git-svn-id: https://svn.lsv.ens-cachan.fr/barbot/Cosmos@1269 c602a718-a262-424a-925f-c628338d67b4

--- trunk/POMDP/main.cpp
+++ trunk/POMDP/main.cpp
@@ -33,119 +33,110 @@ int main(int argc, char** argv) {
     string RewardTrace;
     string st;
     int n,m,k;
-//    vector<int>  W(N,0);//sensor satatus
+	//    vector<int>  W(N,0);//sensor satatus
     
     
     //vector< vector<double> > M = generateMatrix(0);
     
     vector< vector<double> > M;
     
-  
+	
     st=argv[1];
     if(st=="linear"){
-    double p=0.3;
-    double q=0.2;   
-    
-  
-    std::istringstream st( argv[3]);
-    st >> n;
-     
-    M = generateLinearMatrix(n,p,q);
-    ron=-1;
-    rdet=10;
-    ExpFileName="linear.txt";
-    RewardTrace="linear";
-   
+		double p=0.3;
+		double q=0.2;   
+		
+		
+		std::istringstream st( argv[3]);
+		st >> n;
+		
+		M = generateLinearMatrix(n,p,q);
+		ron=-1;
+		rdet=10;
+		ExpFileName="linear.txt";
+		RewardTrace="linear";
+		
     }
     else if(st=="grid"){
-   
-   
-    double u=0.4;
-    double r=0.4;
-    double d=0.1;
-    double l=0.05;
-    
-  
-    std::istringstream st1( argv[3]);
-    st1 >> n;
-    std::istringstream st2( argv[4]);
-    st2 >> m;
-
-    M = generateGridMatrix(n,m,u,r,d,l);
-    ron=-5;
-    rdet=100;
-    
-    ExpFileName="grid.txt";
-    RewardTrace="grid";
-    
-
+		
+		
+		double u=0.4;
+		double r=0.4;
+		double d=0.1;
+		double l=0.05;
+		
+		
+		std::istringstream st1( argv[3]);
+		st1 >> n;
+		std::istringstream st2( argv[4]);
+		st2 >> m;
+		
+		M = generateGridMatrix(n,m,u,r,d,l);
+		ron=-5;
+		rdet=100;
+		
+		ExpFileName="grid.txt";
+		RewardTrace="grid";
+		
+		
     }
     
     else{
-     M = generateMatrix(0);
-     ExpFileName="default.txt";
-     RewardTrace="default";
-     ron=-1;
-     rdet=4;
-
+		M = generateMatrix(0);
+		ExpFileName="default.txt";
+		RewardTrace="default";
+		ron=-1;
+		rdet=4;
+		
     }
- 
+	
     int N = M.size();
     vector< vector< vector<double> > > Plast(N,vector< vector<double> >(Memory, vector<double>(N,1.0)) );
     
     RewardTrace=RewardTrace+argv[2]+".txt";
     ofstream RewardFile(RewardTrace.c_str(), ios::out | ios::trunc);
     ofstream ExpFile(ExpFileName.c_str(), ios::out | ios::app);
-   
-  /*  
-
-    if (argc>1) {
-        double reward = readPlastfun(N, argv[1], Plast);
-        cout << "reward: "<< reward << endl;
-    }
-    
-    generateLHAfun(-0.1,0.4,N,Plast,H,M);
-    generateSPNfun(M);
-    
-   */
-  
-  // initial iteration
-  
-  generateLHAfun(ron,rdet,N,Plast,H,M);
-  generateSPNfun(M);
-  
-  system("Cosmos generated.gspn generated.lha -d test --max-run 100000 --batch 10000");
-  //
-  //loop
-  double Prec=1e-4; //precision
-  double Reward,OldReward;
-  double RelDiff=100; // actual relative difference = abs((Reward-OldReward)/OldReward);
-  int MaxIteration=10;
-  int It=1;
-  
-  system("tail -n 1 test > test2");
-  Reward = readPlastfun(N, "test2", Plast);
-  cout <<"Iteration:" <<It<< " reward: "<< Reward << endl;
-  RewardFile<<It<<" "<<Reward<<endl;
-  while(RelDiff>Prec && It<MaxIteration){
-  //while(It<MaxIteration){
-    It++;    
-    generateLHAfun(ron,rdet,N,Plast,H,M);
-    system("Cosmos generated.gspn generated.lha -d test --max-run 100000 --batch 10000");
-    OldReward=Reward;
-    system("tail -n 1 test > test2");
-    Reward = readPlastfun(N, "test2", Plast);
-    RelDiff=fabs((Reward-OldReward)/OldReward);
-    
-    cout <<"Iteration:" <<It<< " reward: "<< Reward <<" RelDiff: "<<RelDiff<< endl;
-    RewardFile<<It<<" "<<Reward<<endl;
-  }
+	
+	
+	string Cosmoscmd = "Cosmos generated.gspn generated.lha -d test --max-run 100000 --batch 10000";
+	
+	// initial iteration
+	
+	generateLHAfun(ron,rdet,N,Plast,H,M);
+	generateSPNfun(M);
+	
+	system(Cosmoscmd.c_str());
+	//
+	//loop
+	double Prec=1e-4; //precision
+	double Reward,OldReward;
+	double RelDiff=100; // actual relative difference = abs((Reward-OldReward)/OldReward);
+	int MaxIteration=10;
+	int It=1;
+	
+	system("tail -n 1 test > test2");
+	Reward = readPlastfun(N, "test2", Plast);
+	cout <<"Iteration:" <<It<< " reward: "<< Reward << endl;
+	RewardFile<<It<<" "<<Reward<<endl;
+	while(RelDiff>Prec && It<MaxIteration){
+		//while(It<MaxIteration){
+		It++;    
+		generateLHAfun(ron,rdet,N,Plast,H,M);
+		system(Cosmoscmd.c_str());
+		OldReward=Reward;
+		system("tail -n 1 test > test2");
+		Reward = readPlastfun(N, "test2", Plast);
+		RelDiff=fabs((Reward-OldReward)/OldReward);
+		
+		cout <<"Iteration:" <<It<< " reward: "<< Reward <<" RelDiff: "<<RelDiff<< endl;
+		RewardFile<<It<<" "<<Reward<<endl;
+	}
     st=argv[1];
-     if(st=="linear"){
-     ExpFile << "n="<<argv[3]<<" ";
+	if(st=="linear"){
+		ExpFile << "n="<<argv[3]<<" ";
     }
     else if(st=="grid"){
-      ExpFile << "n="<<argv[3]<<" "<< "m="<<argv[4]<<" ";
+		ExpFile << "n="<<argv[3]<<" "<< "m="<<argv[4]<<" ";
     }
     ExpFile <<Reward<<endl;
     
