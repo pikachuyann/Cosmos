c316948722fba54f1958aeb664689992f4c81c97
barbot <barbot@c602a718-a262-424a-925f-c628338d67b4>
2012-10-11 07:40:18 +0000

    set printing of message
    git-svn-id: https://svn.lsv.ens-cachan.fr/barbot/Cosmos@1367 c602a718-a262-424a-925f-c628338d67b4

--- trunk/POMDP/readPlast.cpp
+++ trunk/POMDP/readPlast.cpp
@@ -118,11 +118,11 @@ double readPlastfun(int N,string PlastPath, vector< vector< vector<double> > >&P
 
 void printPlast(  vector< vector< vector<double> > >& Plast ){
 	for (int i= 0; i<Plast.size(); i++){ 
-		cout << i<< "\t[";
+		cout << i<< "\t["<< setprecision(6);
 		for (int j=0; j< Plast[0][0].size(); j++){
 			cout << "->"<<j<<"[";
 			for (int Xlast=0; Xlast<Plast[0].size(); Xlast++){
-				cout<<"\t"<< setw(7)<< Plast[i][Xlast][j];
+				cout<< setw(10)<< Plast[i][Xlast][j];
 			}
 			cout << "]\t";
 		}
