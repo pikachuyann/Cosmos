e01e07913a455231327e2810d88a1a887440b1b6
barbot2 <barbot2@c602a718-a262-424a-925f-c628338d67b4>
2012-11-16 12:59:19 +0000

    improve test teamcity
    git-svn-id: https://svn.lsv.ens-cachan.fr/barbot/Cosmos@1508 c602a718-a262-424a-925f-c628338d67b4

--- trunk/Examples/testTeamCity.ml
+++ trunk/Examples/testTeamCity.ml
@@ -40,6 +40,11 @@ let parse_result f =
        End_of_file -> close_in fs);
   result;;
 
+let invoke_cosmos opt gspn lha  =
+  let cmd = sprintf "../../bin/Cosmos -v 0 --alligator-mode %s %s %s" opt gspn lha in
+  print_endline cmd;
+  Sys.command cmd
+
 let test_result n v =
   let result = parse_result "Result.res" in
   printf "result: %e\nconfint: [%e,%e]\n" result.mean (fst result.confInt) (snd result.confInt);
@@ -50,14 +55,14 @@ let test_result n v =
 let test_cosmos_gspn n v o =
   printf "##teamcity[testStarted name='%s' captureStandardOutput='<true>']\n" n;
   flush stdout;
-  let ret = Sys.command (sprintf "../../bin/Cosmos %s %s.gspn %s.lha" o n n) in
+  let ret = invoke_cosmos o (n^".gspn") (n^".lha") in
   if ret <> 0 then printf "##teamcity[testFailed name='%s' message='Test %s fail: Cosmos return value:%i']\n" n n ret
   else test_result n v;;
 
 let test_cosmos_grml n v o =
   printf "##teamcity[testStarted name='%s' captureStandardOutput='<true>']\n" n;
   flush stdout;
-  let ret = Sys.command (sprintf "../../bin/Cosmos -g %s %s.grml %slha.grml" o n n) in
+  let ret = invoke_cosmos (o^" -g") (n^".grml") (n^"lha.grml")  in
   if ret <> 0 then printf "##teamcity[testFailed name='%s' message='Test %s fail: Cosmos return value:%i']\n" n n ret
   else test_result n v;;
 
