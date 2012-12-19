048bc0ca6e3e8a3ab8e3fcd96ff8247551987a3d
barbot <barbot@c602a718-a262-424a-925f-c628338d67b4>
2012-12-19 16:25:26 +0000

    test coverage
    git-svn-id: https://svn.lsv.ens-cachan.fr/barbot/Cosmos@1612 c602a718-a262-424a-925f-c628338d67b4

--- trunk/Examples/TandemQN/testcoverage.ml
+++ trunk/Examples/TandemQN/testcoverage.ml
@@ -1,5 +1,5 @@
 #use "../testTeamCity.ml";;
 
-test_coverage "tqn" 0.1752 "--max-run 150000 --njob 2 --level 0.99" 500 ;;
-test_coverage "tqn" 0.1752 "--max-run 150000 --njob 2 --level 0.95" 500 ;;
-test_coverage "tqn" 0.1752 "--max-run 150000 --njob 2 --level 0.90" 500 ;;
+test_coverage "tqn" 0.1752 "--max-run 150000 --njob 11 --level 0.99" 5000 ;;
+test_coverage "tqn" 0.1752 "--max-run 150000 --njob 11 --level 0.95" 5000 ;;
+test_coverage "tqn" 0.1752 "--max-run 150000 --njob 11 --level 0.90" 5000 ;;
