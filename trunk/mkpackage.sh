0b36c155f45ab9e3b1115cc9f3c1f36eef205121
barbot <barbot@c602a718-a262-424a-925f-c628338d67b4>
2013-01-16 15:30:37 +0000

    simplify package
    git-svn-id: https://svn.lsv.ens-cachan.fr/barbot/Cosmos@1683 c602a718-a262-424a-925f-c628338d67b4

--- trunk/mkpackage.sh
+++ trunk/mkpackage.sh
@@ -1,29 +1,33 @@
 #!/bin/sh
+make
 mkdir package
-cp Makefile package/
+#cp Makefile package/
 cp cosmosTool package/
 cp Readme.txt package/
 cp COPYING package/
 cp -r SOURCES package/SOURCES
-cp -r prism package/prism
+#cp -r prism package/prism
 find package/SOURCES -name "*.yy" -delete
 find package/SOURCES -name "*.ll" -delete
-mkdir -p package/Obj/ModelGenerator/Eval
-mkdir -p package/Obj/ModelGenerator/GspnParser
-mkdir -p package/Obj/ModelGenerator/LhaParser
-mkdir package/bin
-cp bin/gnuplotScript.gnu package/bin
-cd package
-cd prism; make
-rm -rf classes
-rm -rf cudd
-rm -rf doc
-rm -rf dtds
-rm -rf etc
-rm -rf examples
-rm -rf images
-rm -rf include
-rm -rf obj
-rm -rf src
-cd ..
-make
+find package/SOURCES -name "*.cpp" -delete
+find package/SOURCES -name "*.c" -delete
+find package/SOURCES -name "Makefile" -delete
+#mkdir -p package/Obj/ModelGenerator/Eval
+#mkdir -p package/Obj/ModelGenerator/GspnParser
+#mkdir -p package/Obj/ModelGenerator/LhaParser
+#mkdir package/bin
+cp -r bin package/bin
+#cd package
+#cd prism; make
+#rm -rf classes
+#rm -rf cudd
+#rm -rf doc
+#rm -rf dtds
+#rm -rf etc
+#rm -rf examples
+#rm -rf images
+#rm -rf include
+#rm -rf obj
+#rm -rf src
+#cd ..
+#make
