c6b951241507f34ea9afd2c578b4c8edd9d1cbf1
barbot <barbot@c602a718-a262-424a-925f-c628338d67b4>
2013-03-26 22:09:03 +0000

    improve test
    git-svn-id: https://svn.lsv.ens-cachan.fr/barbot/Cosmos@1951 c602a718-a262-424a-925f-c628338d67b4

--- trunk/Examples/testTeamCity.ml
+++ trunk/Examples/testTeamCity.ml
@@ -47,20 +47,21 @@ let parse_result f =
        End_of_file -> close_in fs);
   result;;
 
-let invoke_cosmos opt gspn lha  =
-  let cmd = sprintf "../../bin/Cosmos -v 0 %s %s %s %s" cosmos_options opt gspn lha in
+let call_cosmos opt =
+   let cmd = sprintf "../../bin/Cosmos -v 0 %s %s" cosmos_options opt in
   print_endline cmd;
   Sys.command cmd
+
+let call_cosmos_silent opt =
+   let cmd = sprintf "../../bin/Cosmos -v 0 %s %s" cosmos_options opt in
+  print_endline cmd;
+  Sys.command cmd
+
+let invoke_cosmos opt gspn lha  =
+  call_cosmos (opt^" "^gspn^" "^lha)
 
 let invoke_cosmos_silent opt gspn lha  =
-  let cmd = sprintf "../../bin/Cosmos -v 0 %s %s %s %s" cosmos_options opt gspn lha in
-  Sys.command cmd
-
-let invoke_cosmos_prism opt gspn lha =
-  let cmd = sprintf "../../bin/Cosmos -v 0 --prism %s %s %s %s > Result.res" cosmos_options opt gspn lha in
-  print_endline cmd;
-  Sys.command cmd
-
+  call_cosmos_silent (opt^" "^gspn^" "^lha)
 
 let test_result n v =
   let result = parse_result "Result.res" in
@@ -69,26 +70,21 @@ let test_result n v =
   then printf "##teamcity[testFinished name='%s' message='expected result %e is in confidence interval |[%e,%e|]']\n" n v (fst result.confInt) (snd result.confInt)
   else printf "##teamcity[testFailed name='%s' message='Test %s fail: expected result %e is outside confidence interval |[%e,%e|]']\n" n n v (fst result.confInt) (snd result.confInt)
 
-let test_cosmos_gspn n v o =
-  printf "##teamcity[testStarted name='%s' captureStandardOutput='<true>']\n" n;
+let test_cosmos testname opt v = 
+   printf "##teamcity[testStarted name='%s' captureStandardOutput='<true>']\n" testname;
   flush stdout;
-  let ret = invoke_cosmos o (n^".gspn") (n^".lha") in
-  if ret <> 0 then printf "##teamcity[testFailed name='%s' message='Test %s fail: Cosmos return value:%i']\n" n n ret
-  else test_result n v;;
+  let ret = call_cosmos opt in
+  if ret <> 0 then printf "##teamcity[testFailed name='%s' message='Test %s fail: Cosmos return value:%i']\n" testname testname ret
+  else test_result testname v;;
+
+let test_cosmos_gspn n v o =
+  test_cosmos n (o^" "^n^".gspn"^" "^n^".lha") v
 
 let test_cosmos_grml n v o =
-  printf "##teamcity[testStarted name='%s' captureStandardOutput='<true>']\n" n;
-  flush stdout;
-  let ret = invoke_cosmos (o^" -g") (n^".grml") (n^"lha.grml")  in
-  if ret <> 0 then printf "##teamcity[testFailed name='%s' message='Test %s fail: Cosmos return value:%i']\n" n n ret
-  else test_result n v;;
-
+  test_cosmos n (o^" -g "^n^".grml"^" "^n^"lha.grml") v
+ 
 let test_cosmos_prism n v o =
-  printf "##teamcity[testStarted name='%s' captureStandardOutput='<true>']\n" n;
-  flush stdout;
-  let ret = invoke_cosmos_prism (o^" -g") (n^".grml") (n^"lha.grml")  in
-  if ret <> 0 then printf "##teamcity[testFailed name='%s' message='Test %s fail: Cosmos return value:%i']\n" n n ret
-  else test_result n v;;
+  test_cosmos n (o^" -g --prism "^n^".grml"^" "^n^"lha.grml") v
 
 let test_coverage name v o n =
   let _ = invoke_cosmos_silent (" --tmp-status 2 --max-run 10 --batch 10 ") (name^".gspn") (name^".lha")  in
