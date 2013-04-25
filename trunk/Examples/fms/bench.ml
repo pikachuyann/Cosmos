bac15c91f21a1bd58338d47b49a3296f3ef4916a
barbot <barbot@c602a718-a262-424a-925f-c628338d67b4>
2013-04-25 07:41:23 +0000

    add geo
    git-svn-id: https://svn.lsv.ens-cachan.fr/barbot/Cosmos@2010 c602a718-a262-424a-925f-c628338d67b4

--- trunk/Examples/fms/bench.ml
+++ trunk/Examples/fms/bench.ml
@@ -17,20 +17,37 @@ let joblist = [
   "S2_102400", "Policy2.grml" , "rho1_102400.grml"]
 
 let joblist2 = [
-  "S1_2_20", "Policy1.grml" , "rho2_10.grml" ;
+  "S1_2_10", "Policy1.grml" , "rho2_10.grml" ;
   "S1_2_50", "Policy1.grml" , "rho2_50.grml" ;
   "S1_2_100", "Policy1.grml" , "rho2_100.grml" ;
   "S1_2_400", "Policy1.grml" , "rho2_400.grml" ;
   "S1_2_6400", "Policy1.grml" , "rho2_6400.grml" ;
   "S1_2_25600", "Policy1.grml" , "rho2_25600.grml" ;
   "S1_2_102400", "Policy1.grml" , "rho2_102400.grml";
-  "S2_2_20", "Policy2.grml" , "rho2_10.grml" ;
+  "S2_2_10", "Policy2.grml" , "rho2_10.grml" ;
   "S2_2_50", "Policy2.grml" , "rho2_50.grml" ;
   "S2_2_100", "Policy2.grml" , "rho2_100.grml" ;
   "S2_2_400", "Policy2.grml" , "rho2_400.grml" ;
   "S2_2_6400", "Policy2.grml" , "rho2_6400.grml" ;
   "S2_2_25600", "Policy2.grml" , "rho2_25600.grml" ;
   "S2_2_102400", "Policy2.grml" , "rho2_102400.grml"]
+
+let joblist3 = [
+  "S1_3_20", "Policy1Geo.grml" , "rho2_10.grml" ;
+  "S1_3_50", "Policy1Geo.grml" , "rho2_50.grml" ;
+  "S1_3_100", "Policy1Geo.grml" , "rho2_100.grml" ;
+  "S1_3_400", "Policy1Geo.grml" , "rho2_400.grml" ;
+  "S1_3_6400", "Policy1Geo.grml" , "rho2_6400.grml" ;
+  "S1_3_25600", "Policy1Geo.grml" , "rho2_25600.grml" ;
+  "S1_3_102400", "Policy1Geo.grml" , "rho2_102400.grml";
+  "S2_3_10", "Policy2Geo.grml" , "rho2_10.grml" ;
+  "S2_3_50", "Policy2Geo.grml" , "rho2_50.grml" ;
+  "S2_3_100", "Policy2Geo.grml" , "rho2_100.grml" ;
+  "S2_3_400", "Policy2Geo.grml" , "rho2_400.grml" ;
+  "S2_3_6400", "Policy2Geo.grml" , "rho2_6400.grml" ;
+  "S2_3_25600", "Policy2Geo.grml" , "rho2_25600.grml" ;
+  "S2_3_102400", "Policy2Geo.grml" , "rho2_102400.grml"]
+
 
 
 let execCosmos (name,model,prop) =
@@ -40,4 +57,4 @@ let execCosmos (name,model,prop) =
   Sys.command (sprintf "cp Result.res %s.res" name);
   ();;
 
-List.iter execCosmos joblist2
+List.iter execCosmos joblist3
