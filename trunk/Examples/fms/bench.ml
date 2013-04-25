2785fe706b42aca48dff6d71cdf70edc8484d566
barbot <barbot@c602a718-a262-424a-925f-c628338d67b4>
2013-04-25 09:47:59 +0000

    fix pb minus
    git-svn-id: https://svn.lsv.ens-cachan.fr/barbot/Cosmos@2013 c602a718-a262-424a-925f-c628338d67b4

--- trunk/Examples/fms/bench.ml
+++ trunk/Examples/fms/bench.ml
@@ -1,20 +1,20 @@
 #use "../testTeamCity.ml";;
 
 let joblist = [
-  "S1_20", "Policy1.grml" , "rho1_20.grml" ;
-  "S1_50", "Policy1.grml" , "rho1_50.grml" ;
-  "S1_100", "Policy1.grml" , "rho1_100.grml" ;
-  "S1_400", "Policy1.grml" , "rho1_400.grml" ;
-  "S1_6400", "Policy1.grml" , "rho1_6400.grml" ;
-  "S1_25600", "Policy1.grml" , "rho1_25600.grml" ;
-  "S1_102400", "Policy1.grml" , "rho1_102400.grml";
-  "S2_20", "Policy2.grml" , "rho1_20.grml" ;
-  "S2_50", "Policy2.grml" , "rho1_50.grml" ;
-  "S2_100", "Policy2.grml" , "rho1_100.grml" ;
-  "S2_400", "Policy2.grml" , "rho1_400.grml" ;
-  "S2_6400", "Policy2.grml" , "rho1_6400.grml" ;
-  "S2_25600", "Policy2.grml" , "rho1_25600.grml" ;
-  "S2_102400", "Policy2.grml" , "rho1_102400.grml"]
+  "S1_1_20", "Policy1.grml" , "rho1_20.grml" ;
+  "S1_1_50", "Policy1.grml" , "rho1_50.grml" ;
+  "S1_1_100", "Policy1.grml" , "rho1_100.grml" ;
+  "S1_1_400", "Policy1.grml" , "rho1_400.grml" ;
+  "S1_1_6400", "Policy1.grml" , "rho1_6400.grml" ;
+  "S1_1_25600", "Policy1.grml" , "rho1_25600.grml" ;
+  "S1_1_102400", "Policy1.grml" , "rho1_102400.grml";
+  "S2_1_20", "Policy2.grml" , "rho1_20.grml" ;
+  "S2_1_50", "Policy2.grml" , "rho1_50.grml" ;
+  "S2_1_100", "Policy2.grml" , "rho1_100.grml" ;
+  "S2_1_400", "Policy2.grml" , "rho1_400.grml" ;
+  "S2_1_6400", "Policy2.grml" , "rho1_6400.grml" ;
+  "S2_1_25600", "Policy2.grml" , "rho1_25600.grml" ;
+  "S2_1_102400", "Policy2.grml" , "rho1_102400.grml"]
 
 let joblist2 = [
   "S1_2_10", "Policy1.grml" , "rho2_10.grml" ;
