9fb69cb613841acaf269f1532167086fda4f6cfe
barbot <barbot@c602a718-a262-424a-925f-c628338d67b4>
2013-09-22 12:47:40 +0000

    add a test for formula
    git-svn-id: https://svn.lsv.ens-cachan.fr/barbot/Cosmos@2218 c602a718-a262-424a-925f-c628338d67b4

--- trunk/Examples/RareEventUnbounded/test.ml
+++ trunk/Examples/RareEventUnbounded/test.ml
@@ -1,3 +1,4 @@
 #use "../testTeamCity.ml";;
 
-test_cosmos_grml "tandem" 0.0089 "--max-run 20000"
+test_cosmos_grml "tandem" 0.0089 "--max-run 20000";;
+test_cosmos "formula" "tandem.grml --formula \"E<100 (Queue2 = 4)\" --max-run 20000 " 0.063742;;
