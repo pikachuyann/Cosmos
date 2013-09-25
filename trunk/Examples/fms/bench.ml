2116b240f10b692b71fb760cd62022bfba39385f
barbot <barbot@c602a718-a262-424a-925f-c628338d67b4>
2013-09-25 08:08:49 +0000

    add date to bench
    git-svn-id: https://svn.lsv.ens-cachan.fr/barbot/Cosmos@2224 c602a718-a262-424a-925f-c628338d67b4

--- trunk/Examples/fms/bench.ml
+++ trunk/Examples/fms/bench.ml
@@ -124,7 +124,7 @@ let joblist5 = [
 
 let execCosmos resultFile csvfile (name,model,prop)  =
   try
-    let r = exec_cosmos model prop 100 8 "--width 0.1" true in
+    let r = exec_cosmos model prop 100 8 "--width 0.005" true in
     ignore (Sys.command (sprintf "mv Result.res %s.res" name));
     output_value resultFile r;
     print_result csvfile "," r;
