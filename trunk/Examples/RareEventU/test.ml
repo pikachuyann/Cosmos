5a7a61dbd27736a02bd89c02c24dd1cddc4d7fb4
Benoit Barbot <barbot@lsv.ens-cachan.fr>
2013-11-20 16:41:39 +0100

    change test value for tandem
    Former-commit-id: acdc4d9993e206c6eb34d2eef5325667fa55e6b6

--- trunk/Examples/RareEventU/test.ml
+++ trunk/Examples/RareEventU/test.ml
@@ -3,10 +3,12 @@
 try ignore (exec_cosmos  "tandem_agr.gspn" "tandem_agr.lha" 1000 1 "-s" true)
 with Sys_error(_) -> ();;
 
-test_cosmos_gspn "tandem" ["",3.687633e-14] "-b 1 --set-Horizon 100 --max-run 5000 --batch 5000";;
-test_cosmos_gspn "tandem" ["",3.687633e-14] "-b 2 --set-Horizon 100 --max-run 5000 --batch 5000";;
-test_cosmos_gspn "tandem" ["",3.687633e-14] "-b 3 --set-Horizon 100 --max-run 5000 --batch 5000";;
+test_cosmos_gspn "tandem" ["",9.95012414790993e-16] "-b 1 --set-Horizon 100 --max-run 5000 --batch 5000";;
+test_cosmos_gspn "tandem" ["",9.95012414790993e-16] "-b 2 --set-Horizon 100 --max-run 5000 --batch 5000";;
+test_cosmos_gspn "tandem" ["",9.95012414790993e-16] "-b 3 --set-Horizon 100 --max-run 5000 --batch 5000";;
+(* Old value : 3.687633e-14 *)
+
+(*
 test_cosmos_gspn "tandem" ["",2.210017e-52] "-b 1 --set-Horizon 10  --max-run 5000 --batch 5000 -c";;
-
 test_cosmos_gspn "tandem"  ["",3.99e-23] "-b 1 --set-Horizon 50  --max-run 500 --batch 500 -c";;
-
+*)
