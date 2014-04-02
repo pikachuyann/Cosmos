bcd4649ab016cc14c801d24fac5f362133adbcc8
Benoit Barbot <barbot@lsv.ens-cachan.fr>
2014-04-02 17:46:12 +0200

    improve bench
    Former-commit-id: e76700f1c1b31aa6925db399ddb0372d7a085037

--- Examples/fms/bench.ml
+++ Examples/fms/bench.ml
@@ -165,16 +165,62 @@ let joblist6 = [
 let add_option (x,y,z) =
   execSavedCosmos ~prefix:false (x,y,z,"--width 0.01 --njob 16")
 
-let lt = [ 100 ; 200; 400; 800; 1600; 6400; 12800; 25600; 102400]
+let lt = [ 10 ; 25 ; (*50;*) 100 ; (*200;*) 400; (*800;*) 1600; (*6400;*) 12800; (*25600;*) 102400 ]
 let lp = [ "Policy1.grml"; "Policy2.grml" ]
+let prop = "rho1.grml"
+let commonopt = "--width 0.01 --njob 1 --batch 100 -v 2 --njob 8 "
 
-let _ = List.iter (fun p ->
+let iterexp name p prop opt =
   List.iter (fun i ->
     execSavedCosmos ~prefix:false (
-    (Printf.sprintf "Table1_%s_%i" p i),
-    p,"rho3_100.grml",
-    (Printf.sprintf "--width 0.01 --njob 1 --const 'T=%i,l1=3,l2=3' -v 1" i ))
-  ) lt) lp
+      (Printf.sprintf "%s_%s_%i" name p i),p,prop,
+      (Printf.sprintf "%s --const 'T=%i'" commonopt i ))
+  ) lt
+
+
+let _ = List.iter (fun p ->
+  iterexp "Table1" p "rho1.grml" (commonopt^" --const 'l1=3,l2=3,failerate=0'") )
+  lp
+
+let _ = List.iter (fun p ->
+  iterexp "Table2" p "rho2.grml" (commonopt^" --const 'l1=3,l2=3,failerate=0'") )
+  lp
+
+(* phi2(sym) *)
+let _ = List.iter (fun p ->
+  iterexp "Table3phi2Sym" p "rho2.grml" (commonopt^" --const 'l1=3,l2=3,failerate=0,mu1=0.476190476190476164,mu2=0.476190476190476164'") ) lp
+
+(* phi2(Asym) S2 *)
+let _ =
+  iterexp "Table3phi2ASym" "Policy2.grml" "rho2.grml" (commonopt^" --const 'l1=3,l2=3,failerate=0,mu1=0.714285714285714302,mu2=0.357142857142857151'")
+
+(* phi2(Asym) S1 *)
+let _ =
+  iterexp "Table3phi2ASym3_5" "Policy1.grml" "rho2.grml" (commonopt^" --const 'l1=3,l2=5,failerate=0,mu1=0.714285714285714302,mu2=0.357142857142857151'")
+
+(* phi2(Asym) S1 *)
+let _ =
+  iterexp "Table3phi2ASym3_4" "Policy1.grml" "rho2.grml" (commonopt^" --const 'l1=3,l2=4,failerate=0,mu1=0.714285714285714302,mu2=0.357142857142857151'")
+
+(* phi2(Asym) S1 *)
+let _ =
+  iterexp "Table3phi2ASym3_3" "Policy1.grml" "rho2.grml" (commonopt^" --const 'l1=3,l2=3,failerate=0,mu1=0.714285714285714302,mu2=0.357142857142857151'")
+
+(* phi2(Asym) S1 *)
+let _ =
+  iterexp "Table3phi2ASym2_3" "Policy1.grml" "rho2.grml" (commonopt^" --const 'l1=2,l2=3,failerate=0,mu1=0.714285714285714302,mu2=0.357142857142857151'")
+
+(* phi2(Asym) S1 *)
+let _ =
+  iterexp "Table3phi2ASym1_3" "Policy1.grml" "rho2.grml" (commonopt^" --const 'l1=1,l2=3,failerate=0,mu1=0.714285714285714302,mu2=0.357142857142857151'")
+
+(* phi2(Asym) S1 *)
+let _ =
+  iterexp "Table3phi2ASym1_4" "Policy1.grml" "rho2.grml" (commonopt^" --const 'l1=1,l2=4,failerate=0,mu1=0.714285714285714302,mu2=0.357142857142857151'")
+
+(* phi2(Asym) S1 *)
+let _ =
+  iterexp "Table3phi2ASym1_5" "Policy1.grml" "rho2.grml" (commonopt^" --const 'l1=1,l2=5,failerate=0,mu1=0.714285714285714302,mu2=0.357142857142857151'")
 
 (*
 let _ = match int_of_string (Sys.argv.(1)) with
