c5b1855bb0a15189e84040fd85a1072590d6f714
Benoit Barbot <barbot@lsv.ens-cachan.fr>
2014-06-10 14:38:50 +0200

    add option for normalization IS
    Former-commit-id: c6ccaef1eadeb37e6fc8cb66fda7ea7c22f9e9e8 [formerly f23c80756294a09a204f765f8c2af35ffad0673f]
    Former-commit-id: 02856255dab019e298f03d368e822b376e30f35b

--- Examples/TandemN/lumpingfun.cpp
+++ Examples/TandemN/lumpingfun.cpp
@@ -7,12 +7,6 @@ void SPN::lumpingFun(const abstractMarking &Marking,vector<int> &vect){
 	vect[0] = Marking.P->_PL_RE_Queue1;
 	vect[1] = Marking.P->_PL_RE_Queue2;
 	vect[2] = Marking.P->_PL_RE_Queue3;
-	vect[3] = Marking.P->_PL_RE_Queue4;
-	vect[4] = Marking.P->_PL_RE_Queue5;
-	vect[5] = Marking.P->_PL_RE_Queue6;
-	vect[6] = Marking.P->_PL_RE_Queue7;
-	vect[7] = Marking.P->_PL_RE_Queue8;
-	vect[8] = Marking.P->_PL_RE_Queue9;
 	int reste =0;
     for (int i=vect.size()-1; i>0; i--) {
         if(vect[i]>r){
