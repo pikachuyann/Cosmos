9a9f2042c8f44178fc9f31811f4a20a64405dbaa
Benoit Barbot <barbot@lsv.ens-cachan.fr>
2014-07-17 13:29:29 +0200

    add benchvar
    Former-commit-id: d4f4b4263c24e2c81b677ff53a46605b66c596ee

--- Examples/TandemN/benchVar.ml
+++ Examples/TandemN/benchVar.ml
@@ -1,6 +1,6 @@
 #use "tandem.ml";;
 
-for i =1 to 70 do
-  let lambda =  0.01*.(float i) in
-  execSavedCosmos ~prefix:false ((Printf.sprintf "Variancevar-%f" lambda),"tandemVar.grml","tandemVar.lha",Printf.sprintf " --width 0 --const \"lambda=%f,rho2=%f\" --max-run 10000000" lambda (0.55-.lambda));
+for i =0 to 20 do
+  let lambda = 0.1 +.  0.02*.(float i) in
+  execSavedCosmos ~prefix:false ((Printf.sprintf "Variancevar-%f" lambda),"tandemVar.grml","tandemHisto.lha",Printf.sprintf " --width 0 --output-PDFCDF pdf_%f.dat  --const \"lambda=%f,rho2=%f\" --max-run 1000000000 --njob 12" lambda lambda (0.55-.lambda));
 done;;
