d63f8535b4eead3a48da5db933c19071333c4db0
Benoit Barbot <benbot@clpc29.cs.ox.ac.uk>
2014-10-08 18:11:49 +0100

    trqck 28
    Former-commit-id: c9fc03c1b58b31439ce99347b6278c6a2b5b18cf

--- Examples/DNAWalker/test.sh
+++ Examples/DNAWalker/test.sh
@@ -1,1 +1,3 @@
-../../bin/Cosmos test.grml --sampling 5000 10.0 --trace-place "a1,a2,a3,a4,a9,a10,a11,a12,a8" --output-PDFCDF test.dat --njob 8 --max-run 100000
+Cosmos track18Block1.grml --sampling 5000 10.0 --trace-place "a1,a2,a3,a4,a9,a10,a11,a12,a8" --output-PDFCDF test.dat --njob 8 --max-run 100000
+
+Cosmos track28.grml --formula "(a20=0 & a25=0 & a28=0)  U<12000 a17>0" --njob 8 --level 0.95 --gppcmd clang++ --max-run 100000000
