a7137c3412c404ffba07b2963d22474cf1c5c3d0
Benoit Barbot <benbot@cs.ox.ac.uk>
2014-10-15 09:26:27 +0100

    add bench example caml
    Former-commit-id: 8d02d19440a6ce43a7f85fdcb992c6a6ee46fd77 [formerly 7e80377f79c3131793dfd205c686ecbe86f4d478]
    Former-commit-id: f95eb0c937e3c188a5a5fefbeb504a20053f29a5

---
+++ Examples/bench.ml
@@ -0,0 +1,52 @@
+#use "testTeamCity.ml";;
+
+let log = (Unix.getcwd ())^"/cosmos_test_log";;
+
+init_log log;;
+
+let benchlist = [
+  "TandemQN", None;
+  "pi",None;
+  "testDistribution",None;
+  "Workflow-Net",None;
+  "QueueMM1", None;
+  "signallingCascade", None;
+  "DataBase" , None;
+  "RareEventUnbounded", None;
+  "RareEventUnbounded", Some("test2.ml");
+  "TandemCosy", Some("testPrism.ml");
+  "SharedMemory" , None;
+  "RareEventU", None;
+  "DNAWalker", None;
+	 
+ ];;
+
+List.iter (fun (s,n) -> 
+  let cmd = Printf.sprintf "cd %s && ocaml %s bash %s" s (
+    match n with None->"test.ml" | Some c ->c) log in
+  ignore (Sys.command cmd)) benchlist;;
+
+let (succ,failure) = read_log log in
+Printf.printf "Test finished: %i success and %i failure\n" (List.length succ)
+  (List.length failure);
+if List.length failure >0 then (
+  Printf.printf "The following test failed:\n";
+  List.iter print_endline failure
+)
+
+(*
+
+	(cd TandemQN && ocaml test.ml $(INTC))
+	(cd pi && ocaml test.ml $(INTC))
+	(cd testDistribution && ocaml test.ml $(INTC))
+	(cd Workflow-Net && ocaml test.ml $(INTC))
+	(cd QueueMM1 && ocaml test.ml $(INTC))
+	(cd signallingCascade && ocaml test.ml $(INTC))
+	(cd DataBase && ocaml test.ml $(INTC))
+	(cd RareEventUnbounded && ocaml test.ml $(INTC))
+#	(cd RareEventUnbounded && ocaml test2.ml $(INTC))
+#	(cd TandemCosy && ocaml testPrism.ml $(INTC))
+	(cd SharedMemory && ocaml test.ml $(INTC))
+	(cd RareEventU && ocaml test.ml $(INTC))
+	(cd DNAWalker && ocaml test.ml $(INTC))
+*)
