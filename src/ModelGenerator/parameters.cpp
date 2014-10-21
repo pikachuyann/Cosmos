6c2c0eedb62690a5f7b4761cf1f931769d2ec05a
Benoit Barbot <benbot@cs.ox.ac.uk>
2014-10-21 18:48:56 +0100

    improve gnuplot driver
    Former-commit-id: 6b9e7793fb851748d58f8719a7e07033b27bbe30 [formerly d0ed904ef0fe9ae72a724a8c3c3d5643b3ffbac9]
    Former-commit-id: 5cdf5681b2ad951edeb01864cf559d82e1457848

--- src/ModelGenerator/parameters.cpp
+++ src/ModelGenerator/parameters.cpp
@@ -90,7 +90,7 @@ dataraw(""),
 datatrace(""),
 sampleResol(0.0),
 dataPDFCDF(""),
-gnuplotDriver(false),
+gnuplotDriver(""),
 tracedPlace("ALL"),
 dotfile(""),
 
@@ -156,7 +156,7 @@ void parameters::usage() {
     cout << "\t--output-trace arg resol \tOutput the trace each trajectory in the file arg with a resolution of resol" << endl;
     cout << "\t--output-graph \tOutput the result of CDF or PDF formula in gnuplot file format" << endl;
     cout << "\t--output-dot \tOutput the Petri net in a dot file" << endl;
-    cout << "\t--gnuplot-driver \tRun gnuplot on the output datafile to produce graph" << endl;
+    cout << "\t--gnuplot-driver terminal\tRun gnuplot on the output datafile to produce graph use the given terminal for gnuplot" << endl;
     cout << "\t--trace-pt arg\tSpecify which place to trace in all the output file, arg is a comma separated list of places and transitions name" << endl;
     cout << "\t--HASL-expression \tAllow to define an HASL formula from the command line" << endl;
     cout << "\t--loop t1 [--transtient t2] \tGenerate an LHA that loop for t1 times unit and then t2 time unit. The --transient option alone do not do anything" << endl;
@@ -224,7 +224,7 @@ void parameters::parseCommandLine(int argc, char** argv) {
             {"output-PDFCDF", required_argument, 0, 11},
             {"output-graph", required_argument, 0, 11},
             {"output-dot", required_argument, 0, 28},
-            {"gnuplot-driver", no_argument, 0, 15},
+            {"gnuplot-driver", required_argument, 0, 15},
             {"trace-place", required_argument, 0, 19},
             {"trace-pt", required_argument, 0, 19},
             {"help", no_argument, 0, 'h'},
@@ -403,7 +403,7 @@ void parameters::parseCommandLine(int argc, char** argv) {
             case 'f': CSLformula = optarg;
                 break;
 
-            case 15: gnuplotDriver = true;
+            case 15: gnuplotDriver = optarg;
                 break;
 
             case 19: tracedPlace = optarg;
