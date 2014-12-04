8f1a090155954535014538cab6a473c09f261932
Benoît Barbot <barbot@plonk-3.local>
2014-12-04 18:48:23 +0000

    prog
    Former-commit-id: a7598231228df9766e2ef2a5b43116d2d0a902b7 [formerly 321ff67d7adad5dc3752294ba3d43ebfc26ac26e]
    Former-commit-id: 764b89525df39696a499fe9a931c82b197a5ae3e

--- prism2SPT/build.sh
+++ prism2SPT/build.sh
@@ -1,1 +1,2 @@
+rm -rf tmp
 ocamlbuild -cflags -I,+xml-light,-I,/Users/barbot/.opam/system/lib/zip -lflags -I,+xml-light,-I,/Users/barbot/.opam/system/lib/zip/ -libs unix,xml-light,zip,Str prism2SPT.native
