ff57c92cddfca3d906485f712bd337246ed9b8b7
Alexandru Mereacre <mereacre@gmail.com>
2015-04-09 17:59:03 +0100

    Loop param
    Former-commit-id: 6217958e2a59b726c4935da7805052fdee5e56cc

--- src/syn/syn.py
+++ src/syn/syn.py
@@ -1,5 +1,4 @@
 import monsoon
-
 import sys
 import threading
 import time
@@ -12,27 +11,177 @@ import random
 import ctypes
 import numpy as np
 
+from fparams import isParamValuationFeasible
+
 exitFlag = 1
 stDataColl = 0
 collectedSamples = []
 useVM = 0
-logTime = 2 # in Seconds
+logTime = 10 # in Seconds
+
+def SetArduinoParameter(handle, parID, parValue):
+		headerID = 0xF5
+
+		print "Sending Arduino parameter ID: "+str(parID)+" value: "+str(parValue)
+
+		if SetParameter(handle, headerID, parID, parValue)!=1:
+			return 0
+
+		return 1
+
+def SetClientParameter(handle, parID, parValue):
+		headerID = 0xF4
+
+		print "Sending Client parameter ID: "+str(parID)+" value: "+str(parValue)
+
+		if SetParameter(handle, headerID, parID, parValue)!=1:
+			return 0
+
+		return 1
+
+def SetParameter(handle, headerID, parID, parValue):
+		parStr = struct.pack('BBI',headerID, parID, parValue)
+
+		# Send parameter
+		handle.sendall(parStr)
+
+		buftmp = bytearray(1)
+		buflen = handle.recv_into(buftmp,1)
+
+		if buftmp[0]!=0xF6:
+			print "Wrong return value"
+			return 0
+
+		return 1
+
+def SaveMarkersToFile(handle, dta):
+	if dta==0:
+		handle.write(str(0)+" "+str(0))
+	elif dta==1:
+		handle.write(str(1)+" "+str(0))
+	elif dta==2:
+		handle.write(str(0)+" "+str(1))
+	elif dta==3:
+		handle.write(str(1)+" "+str(1))
 
 def SaveToFile(handle, dta):
-	if dta==0:
-		handle.write(str(0)+" "+str(0)+"\n")
-	elif dta==1:
-		handle.write(str(1)+" "+str(0)+"\n")
-	elif dta==2:
-		handle.write(str(0)+" "+str(1)+"\n")
-	elif dta==3:
-		handle.write(str(1)+" "+str(1)+"\n")
+	SaveMarkersToFile(handle, dta)
+	handle.write("\n")
 
-def GetEnergyReadings(pmData, monitorSamplingFreq, tranListTimes):
+def SaveToFileAll(handle, dta, current):
+	SaveMarkersToFile(handle, dta)
+	handle.write(" "+str(current))
+	handle.write("\n")
+
+def GetMinCurrent(pmData):
+	currList = [it[0] for it in pmData]
+
+	return min(currList)
+
+def GetEnergyReadings(pmData, monitorSamplingFreq, tranListTimes, minCurrent):
+	cummCurrentList = []
+
 	sumCurrent = 0
 	sampleCnt = 1
 	prev = pmData[0][1]
 	
+	timeList = [item[1] for item in tranListTimes]
+	cummCurrentList = [[item[0],item[1],0] for item in tranListTimes]
+
+	it_beg = 0
+
+	for it in range(1,len(pmData)):
+		if pmData[it][1]!=2 and prev==2:
+			it_beg = it
+			break
+
+	tmpSampleCnt = 0
+	timeListIdx = 0
+
+	for it in range(it_beg,len(pmData)):
+		stime = sampleCnt/monitorSamplingFreq
+		sumCurrent = sumCurrent + pmData[it][0]
+		tmpSampleCnt = tmpSampleCnt + 1
+
+		if timeListIdx>=len(timeList):
+			break
+
+		if stime>=timeList[timeListIdx]:
+			cummCurrentList[timeListIdx][2] = sumCurrent - tmpSampleCnt * minCurrent
+			timeListIdx = timeListIdx + 1
+			sumCurrent = 0
+			tmpSampleCnt = 0
+
+		sampleCnt = sampleCnt + 1
+
+	
+	filedta = open('markers.txt', 'w+')
+
+	for it in range(it_beg,len(pmData)):
+		SaveToFileAll(filedta, pmData[it][1], pmData[it][0])
+
+	filedta.close()
+	return cummCurrentList
+
+def GetTotalCurrents(handle, esamples, monitorSamplingFreq):
+		cummCurrentList = []
+
+		# Send get list of IDs
+		handle.sendall('\xF3')
+
+		bufSize = bytearray(4)
+		buflen = handle.recv_into(bufSize, 4)
+
+		if buflen!=4:
+			print "Wrong packet length"
+			return cummCurrentList
+
+		bufSize = struct.unpack("I",bufSize)
+
+		print "Packet size: "+str(bufSize[0])
+
+		bufIDs = bytearray()
+		buftmp = bytearray(2048)
+		bytes_recd = 0
+
+		while bytes_recd < bufSize[0]:
+			chunkLen = handle.recv_into(buftmp,2048)
+			bufIDs[bytes_recd:] = buftmp[:chunkLen]
+			bytes_recd = bytes_recd + chunkLen
+
+		if 	bytes_recd != bufSize[0]:
+			print "Wrong data"
+			return cummCurrentList
+
+		nTranCnt = 	bufSize[0]/8
+		print "Number of transitions received: "+str(nTranCnt)
+
+		#if bufSize[0]!=len(currentTran):
+		#	print "Diffent number of samples(client, power monitor)"
+		#	break
+
+		#print binascii.hexlify(bufIDs)
+		
+		tTimeList = []
+		for idx in range(0,nTranCnt):
+			tID = bufIDs[idx*8]
+			tTime = struct.unpack("I",bufIDs[idx*8+1:idx*8+1+4])
+			tTimeList.append([tID, tTime[0]])
+			print str(tTime[0])+" "+str(bufIDs[idx*8])
+
+		toRemove = []	
+		for idx in range(len(tTimeList)-1):
+			if tTimeList[idx][1]==tTimeList[idx+1][1]:
+				toRemove.append(tTimeList[idx])
+
+		for rem in toRemove:
+			tTimeList.remove(rem)
+
+		minCurrent = GetMinCurrent(esamples)
+		cummCurrentList = GetEnergyReadings(esamples, monitorSamplingFreq, tTimeList, minCurrent)
+
+		return cummCurrentList
+
 def GetRisingEdge(pmData):
 	cnt = 0
 	prev = pmData[0][1]
@@ -235,6 +384,9 @@ if s is None:
 #s.setblocking(0)
 
 idCurr = []
+# 0 = ID, 1 = min, 2 = max, 3 = set value
+parVals = [[0, 300, 2000, 1000], [1, 20000, 10400], [2, 100, 30000, 3000], [0, 300, 2000, 500], [1, 0, 200, 131], [2, 200, 1200, 1000]]
+parDict = {'SA_d':0, 'SA_ectopD':1, 'VRG_d':2, 'TURI':3, 'TAVI':4, 'TLRI':5}
 
 if useVM==0:
 	print "Preparing the PowerMonitor device..."
@@ -242,28 +394,24 @@ if useVM==0:
 
 	for iters in range(0, 1):
 
-		# Generate random parameter
-		headerID = 0xF4
-		parID = 1
-		parValue = random.randint(1, 2000)
-		#parValue = 809 		# Very slow Sa-node rate
-		parValue = 30000
-		#parValue = 177
+		# Generate random parameter for Client
+		parValue = random.randint(parVals[parDict["SA_d"]][1], parVals[parDict["SA_d"]][2])
+		parVals[parDict["SA_d"]][3] = parValue
+		if SetClientParameter(s, parVals[parDict["SA_d"]][0], parValue)!=1:
+			break;
 
-		print "Sending parameter ID: "+str(parID)+" value: "+str(parValue)
+		parValue = random.randint(parVals[parDict["TURI"]][1], parVals[parDict["TURI"]][2])
+		parll = [parVals[parDict["TLRI"]][3]-parVals[parDict["TAVI"]][3], parValue]
 
-		parStr = struct.pack('BBI',headerID, parID, parValue)
+		if isParamValuationFeasible(parll)!=1:
+			print "Parameter not feasible: "+str(parll)
+			#break
 
-		# Send parameter to Client
-		s.sendall(parStr)
-
-		buftmp = bytearray(1)
-		buflen = s.recv_into(buftmp,1)
-
-		if buftmp[0]!=0xF6:
-			print "Wrong return value"
+		parVals[parDict["TURI"]][3] = parValue
+		if SetArduinoParameter(s, parVals[parDict["TURI"]][0], parValue)!=1:
 			break
-
+	
+		#isParamValuationFeasible(param)	
 		print "Start iteration: "+str(iters)
 
 		# Start iteration
@@ -279,7 +427,7 @@ if useVM==0:
 
 		print "Stopped collecting data"
 
-		currentTran = ProcessPowerMonitorData(collectedSamples, monItems['sampleRate'])
+		#currentTran = ProcessPowerMonitorData(collectedSamples, monItems['sampleRate'])
 		#idx = -1
 
 		#for idx in range(len(currentTran)-1,-1,-1):
@@ -297,52 +445,17 @@ if useVM==0:
 		#nrising = GetRisingEdge(collectedSamples)
 		#print "Numbert of rising edges: "+str(nrising)
 
-		# Send get list of IDs
-		s.sendall('\xF3')
 
-		bufSize = bytearray(4)
-		buflen = s.recv_into(bufSize, 4)
+		#print cummCurrentList
 
-		if buflen!=4:
-			print "Wrong packet length"
+		cummCurrentList = GetTotalCurrents(s, collectedSamples, monItems['sampleRate'])
+
+		if len(cummCurrentList)==0:
 			break
-
-		bufSize = struct.unpack("I",bufSize)
-
-		print "Packet size: "+str(bufSize[0])
-
-		bufIDs = bytearray()
-		buftmp = bytearray(2048)
-		bytes_recd = 0
-
-		while bytes_recd < bufSize[0]:
-			chunkLen = s.recv_into(buftmp,2048)
-			bufIDs[bytes_recd:] = buftmp[:chunkLen]
-			bytes_recd = bytes_recd + chunkLen
-
-		if 	bytes_recd != bufSize[0]:
-			print "Wrong data"
-			break
-
-		nTranCnt = 	bufSize[0]/8
-		print "Number of transitions received: "+str(nTranCnt)
-
-		#if bufSize[0]!=len(currentTran):
-		#	print "Diffent number of samples(client, power monitor)"
-		#	break
-
-		#print binascii.hexlify(bufIDs)
-		
-		tTimeList = []
-		for idx in range(0,nTranCnt):
-			tID = bufIDs[idx*8]
-			tTime = struct.unpack("I",bufIDs[idx*8+1:idx*8+1+4])
-			tTimeList.append(tTime[0])
-			print str(tTime[0])+" "+str(bufIDs[idx*8])
 
 		# Save energy readings to list	
-		#for idx in range(bufSize[0]):
-		#	idCurr.append([bufIDs[idx],currentTran[idx][0]])
+		for item in cummCurrentList:
+			idCurr.append([item[0],item[2]])
 
 		collectedSamples = []
 
