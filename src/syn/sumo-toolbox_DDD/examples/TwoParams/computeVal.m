b791aeb78870b69e4ba112397827f4d8449a0423
Alexandru Mereacre <mereacre@gmail.com>
2015-05-01 15:59:12 +0100

    New changes
    Former-commit-id: 8b411522c43f38a65166d20a6670601bbed35117

--- src/syn/sumo-toolbox_DDD/examples/TwoParams/computeVal.m
+++ src/syn/sumo-toolbox_DDD/examples/TwoParams/computeVal.m
@@ -6,36 +6,38 @@ global outFile;
 global inFile;
 global currentSamples;
 
-if currentSampleNum == 0
-    fid_out = fopen(outFile,'w');
-else
-    fid_out = fopen(outFile,'a');
-end
-fprintf(fid_out,'%f,',x(:,1));
-fprintf(fid_out,'%f\n',x(:,2));
-fclose(fid_out);
+% if currentSampleNum == 0
+%     fid_out = fopen(outFile,'w');
+% else
+%     fid_out = fopen(outFile,'a');
+% end
+
+fprintf(outFile,'%d,',ceil(x(:,1)));
+fprintf(outFile,'%d\n',ceil(x(:,2)));
+% fclose(fid_out);
 disp(strcat(sprintf('%d',currentSampleNum+1),'-th sample. Value: ',sprintf('%f,%f',x)));
-feasible = saferegion(x);
-if feasible>0
-    disp('Point is not feasible');
-else
-    disp('Point is feasible');
-end
+% feasible = saferegion(x);
+% if feasible>0
+%     disp('Point is not feasible');
+% else
+%     disp('Point is feasible');
+% end
 
 disp('Waiting for simulation results...');
-pause(5);%wait 5 seconds
-fid_in=fopen(inFile,'r');
-fid_in=readLines(fid_in,currentSampleNum);
-tline = fgetl(fid_in);
-fclose(fid_in);
+% pause(5);%wait 5 seconds
+% fid_in=fopen(inFile,'r');
+% fid_in=readLines(fid_in,currentSampleNum);
+
+tline = fgetl(inFile);
+% fclose(fid_in);
 %until results are written, wait 1 second and try to read
-while ~ischar(tline)
-    pause(1);
-    fid_in=fopen(inFile,'r');
-    fid_in=readLines(fid_in,currentSampleNum);
-    tline = fgetl(fid_in);
-    fclose(fid_in);
-end
+% while ~ischar(tline)
+%     pause(1);
+%     fid_in=fopen(inFile,'r');
+%     fid_in=readLines(fid_in,currentSampleNum);
+%     tline = fgetl(fid_in);
+%     fclose(fid_in);
+% end
 f=sscanf(tline,'%f');
 disp(strcat('Result: ',tline));
 currentSampleNum=currentSampleNum+1;
--- src/syn/sumo-toolbox_DDD/examples/TwoParams/safeRegPM.m
+++ src/syn/sumo-toolbox_DDD/examples/TwoParams/safeRegPM.m
@@ -2,9 +2,8 @@ function y = safeRegPM(x)
 global safeReg;
 p1_dis = ceil(x(:,1)/10);
 p2_dis = ceil(x(:,2)/10);
-reg=safeReg.paramSpace;
 y= p1_dis;
 for i=1:size(y,1)
-    y(i) = (p1_dis(i)>size(reg,1) || p2_dis(i)>size(reg,2) || reg(p1_dis(i),p2_dis(i))>0)*2000;
+    y(i) = (p1_dis(i)>size(safeReg,1) || p2_dis(i)>size(safeReg,2) || safeReg(p1_dis(i),p2_dis(i))>0)*2000;
 end
 end
