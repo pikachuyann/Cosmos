67283248839275c596184d758e16d4e686837539
Nicola Paoletti <paoletti@Nicola-Pao-MacBook-Pro.local>
2015-05-12 10:55:20 +0100

    added updated bb controller sumo
    Former-commit-id: 921ed5edf202b18635f4eef6aeed38b69cb48941

---
+++ src/syn/sumo-toolbox_DDD/experiments/BBcontroller_TON_new.m
@@ -0,0 +1,25 @@
+sumoPath='~/Projects/Cosmos/src/syn/sumo-toolbox_DDD/';
+pyPath='~/Projects/pacemaker/PaceMakerModel/';
+%sumoPath='~/work/gitFolders/Cosmos/src/syn/sumo-toolbox_DDD/';
+%pyPath='~/work/gitFolders/pacemaker/PaceMakerModel/';
+
+cd(sumoPath);
+mat2py = strcat(pyPath,'mat2py.dat');
+py2mat = strcat(pyPath,'py2mat.dat');
+
+% default
+saferegscript = 'saferegion';
+% PM
+% saferegscript = 'safeRegPM';
+nParams=1;
+ranges = [1,103];
+paramNames = {'TON'};
+safeRegionFile = 'experiments/safe_regions/bbcontroller_ton_new.mat';
+experimentName = 'bbcontroller-ton-new';
+numInitSamples = 10;
+numTotalSamples = 100;
+
+
+startSumoOptimization(mat2py, py2mat, saferegscript, ...
+    nParams, ranges, paramNames, safeRegionFile, experimentName, ...
+    numInitSamples, numTotalSamples) 
