ea46d40691ab747ff4a16c8e2dc8e051dec4c2cc
Benoit Barbot <benbot@cs.ox.ac.uk>
2015-05-14 10:51:01 +0100

    fix conflict
    Former-commit-id: 23dfdc39eb4263bc726fa3f796e032db7fe61998 [formerly 656aa3039683812228c7b557dad2cc53e6bb73b1]
    Former-commit-id: 3a527ad289ef717e3b441ee73344ea8490469cec

--- src/LightSimulator/build.sh
+++ src/LightSimulator/build.sh
@@ -41,7 +41,7 @@ cd sketchArduino
 
 SERIAL_FIO=`ls /dev/tty.usbserial-* /dev/ttyUSB* 2>/dev/null`
 SERIAL_UNO=`ls /dev/tty.usbmodemfd* 2>/dev/null`
-#SERIAL_BLEND=`ls /dev/tty.usbmodem* /dev/ttyACM* 2>/dev/null`
+SERIAL_BLEND=`ls /dev/tty.usbmodem* /dev/ttyACM* 2>/dev/null`
 
 if [[ -n $SERIAL_BLEND ]]; then
     BOARD=blendmicro8
