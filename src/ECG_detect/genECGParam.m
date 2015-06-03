2531dfb533ce2a342aa1f042924902eabbba2ea0
Benoit Barbot <benbot@cs.ox.ac.uk>
2015-06-03 18:00:39 +0100

    improve ecg gen
    Former-commit-id: 461008df7a0ea8fc324ca95592ec3de50d0b0acb [formerly ea2f022c5acbefb404a66c00b0ea7d2f90bd9bd4]
    Former-commit-id: ccfd8fff9453cbeaf00de70c9961f56e4b494a32

--- src/ECG_detect/genECGParam.m
+++ src/ECG_detect/genECGParam.m
@@ -1,8 +1,8 @@
-function [ ECGParams,RTdelay,RRdelay,PQdelay,QRdelay ] = genECGParam( EKG,rate,time )
+function [ ECGParams] = genECGParam( EKG,rate,time )
 %GENECGPARAM Summary of this function goes here
 %   Detailed explanation goes here
-[R_data,S_data,T_data,Q_data,P_data,PQ_i, QR_i,RT_i,RR_i, ectopic, S_data_filtered, Q_data_filtered, heart_rate,buffer_plot]=peakdetect_offline(EKG,rate,time);
-[ dT2 , hT2, wT2] = extractDelay( R_data,S_data,T_data,Q_data,P_data,rate)
+[R_data,S_data,T_data,Q_data,P_data,PQ_i, QR_i,RT_i,RR_i, ectopic, R_filtered, P_filtered, T_filtered, S_filtered, Q_filtered, heart_rate,buffer_plot]=peakdetect_offline(EKG,rate,time);
+[ dT2 , hT2, wT2] = extractDelay( R_filtered,S_filtered,T_filtered,Q_filtered,P_filtered,rate)
 
 index=19
 
@@ -10,19 +10,17 @@ dT=dT2
 hT=hT2
 wT=wT2
 
-RRdelay = dT(index,1)*1000;
-PQdelay = dT(index,2)*1000;
-QRdelay = (dT(index,3)-dT(index,2))*1000;
-Sdelay =  (dT(index,4)-dT(index,3))*1000;
-RTdelay = (dT(index,5)-dT(index,3))*1000;
+RRdelay = dT(:,1)*1000;
+PQdelay = dT(:,2)*1000;
+QRdelay = (dT(:,3)-dT(:,2))*1000;
+Sdelay =  (dT(:,4)-dT(:,3))*1000;
+RTdelay = (dT(:,5)-dT(:,3))*1000;
 
-delay = [1.000000000000000;
-         1.00000000000000;
-         1.000000000000000;
-         1.00000000000000 + Sdelay/1000 ;
-         1.000000000000000];
+delay = [RRdelay PQdelay QRdelay Sdelay RTdelay];
 
-ECGParams =  [hT(index,:)' (wT(index,:)') delay];
+%ECGParams =  [ hT(index,:)' (wT(index,:)') (delay(index,:)') ];
+
+ECGParams =  [ hT wT delay];
 
 end
 
