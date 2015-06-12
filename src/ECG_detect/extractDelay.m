26eef6dfaf2b4ef73874cc3fe0c31528dd50d751
Benoit Barbot <benbot@cs.ox.ac.uk>
2015-06-12 12:32:13 +0100

    fix
    Former-commit-id: d4b85dd26815bf1057fe82fa242dc6a400694235

--- src/ECG_detect/extractDelay.m
+++ src/ECG_detect/extractDelay.m
@@ -1,18 +1,35 @@
-function [ delayTab, heightTab, widthTab] = extractDelay( R_data,S_data,T_data,Q_data,P_data,rate )
+function [ delayTab, heightTab, widthTab] = extractDelay( R_data,S_data,S_filtered_data,T_data,Q_data,P_data,rate )
 %EXTRACTDELAY Summary of this function goes here
 %   Detailed explanation goes here
 
-minT = P_data(1,1);
-maxT = T_data(length(T_data),1);
+% minT = P_data(1,1);
+% maxT = T_data(length(T_data),1);
 
-P_data = filterOut(minT,maxT,P_data);
-Q_data = filterOut(minT,maxT,Q_data);
-R_data = filterOut(minT,maxT,R_data);
-S_data = filterOut(minT,maxT,S_data);
-T_data = filterOut(minT,maxT,T_data);
+% P_data = filterOut(minT,maxT,P_data);
+% Q_data = filterOut(minT,maxT,Q_data);
+% R_data = filterOut(minT,maxT,R_data);
+% S_data = filterOut(minT,maxT,S_data);
+% T_data = filterOut(minT,maxT,T_data);
 
+S_data_p=[];
+for i=1:size(T_data,1)
+    idx = find(S_data(:,2)==T_data(i,2),1);
+    S_data_p = [S_data_p S_data(idx,:)];
+end
+    
+S_width_filtered = S_data(:,4);
+for i=1:length(S_width_filtered)
+   idx=find(S_filtered_data(:,2)==S_data(i,2),1);
+   if ~isempty(idx)
+   S_width_filtered(i)=S_filtered_data(idx,4);
+   end
+end
 
-
+size(P_data,1)
+size(Q_data,1)
+size(R_data,1)
+size(S_data,1)
+size(T_data,1)
 
 delayTab = [ (P_data(1:end,1) - [0.0; P_data(1:end-1,1)]) ...
             ,(Q_data(:,1)-P_data(:,1)) ...
@@ -20,17 +37,17 @@ delayTab = [ (P_data(1:end,1) - [0.0; P_data(1:end-1,1)]) ...
             ,(S_data(:,1)-P_data(:,1)) ...
             ,(T_data(:,1)-P_data(:,1))]/rate;
         
-heightTab = [ P_data(:,2) ...
-            , Q_data(:,2) ...
-            , R_data(:,2) ...
-            , S_data(:,2) ...
-            , T_data(:,2) ];
-   
-widthTab  = [ P_data(:,3) ...
+heightTab = [ P_data(:,3) ...
             , Q_data(:,3) ...
             , R_data(:,3) ...
             , S_data(:,3) ...
-            , T_data(:,3) ]/rate;
+            , T_data(:,3) ];
+   
+widthTab  = [ P_data(:,4) ...
+            , Q_data(:,4) ...
+            , R_data(:,4) ...%, S_data(:,3) ...
+            , S_width_filtered ...
+            , T_data(:,4) ]/rate;
         
 end
 
--- src/ECG_detect/genECGParam.m
+++ src/ECG_detect/genECGParam.m
@@ -2,7 +2,7 @@ function [ ECGParams] = genECGParam( EKG,rate,time )
 %GENECGPARAM Summary of this function goes here
 %   Detailed explanation goes here
 [R_data,S_data,T_data,Q_data,P_data,PQ_i, QR_i,RT_i,RR_i, ectopic, R_filtered, P_filtered, T_filtered, S_filtered, Q_filtered, heart_rate,buffer_plot]=peakdetect_offline(EKG,rate,time);
-[ dT2 , hT2, wT2] = extractDelay( R_filtered,S_filtered,T_filtered,Q_filtered,P_filtered,rate)
+[ dT2 , hT2, wT2] = extractDelay( R_filtered,S_data,S_filtered,T_filtered,Q_filtered,P_filtered,rate)
 
 index=19
 
