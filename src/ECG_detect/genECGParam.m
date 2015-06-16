ae12ec8c922ff71a096a7e715e074848c3a1ee83
Nicola Paoletti <paoletti@Nicola-Pao-MacBook-Pro.local>
2015-06-16 16:13:03 +0100

    .
    Former-commit-id: c4379d8f8b568dc85cd5efdd3189de5fa8bd6a51

--- src/ECG_detect/genECGParam.m
+++ src/ECG_detect/genECGParam.m
@@ -1,14 +1,17 @@
-function [ ECGParams] = genECGParam( EKG,rate,time )
+function [ECGParams, filteredECG] = genECGParam( EKG,rate,time,plot )
 %GENECGPARAM Summary of this function goes here
 %   Detailed explanation goes here
-[R_data,S_data,T_data,Q_data,P_data,PQ_i, QR_i,RT_i,RR_i, ectopic, R_filtered, P_filtered, T_filtered, S_filtered, Q_filtered, heart_rate,buffer_plot]=peakdetect_offline(EKG,rate,time);
-[ dT2 , hT2, wT2] = extractDelay( R_filtered,S_data,S_filtered,T_filtered,Q_filtered,P_filtered,rate)
+if nargin < 4
+    plot = false;
+end
+[R_data,S_data,T_data,Q_data,P_data,PQ_i, QR_i,RT_i,RR_i, ectopic, R_filtered, P_filtered, T_filtered, S_filtered, Q_filtered, heart_rate,filteredECG]=peakdetect_offline(EKG,rate,time,plot);
+[ dT2 , hT2, wT2] = extractDelay( R_filtered,S_data,S_filtered,T_filtered,Q_filtered,P_filtered,rate);
 
-index=19
+% index=19;
 
-dT=dT2
-hT=hT2
-wT=wT2
+dT=dT2;
+hT=hT2;
+wT=wT2;
 
 RRdelay = dT(:,1)*1000;
 PQdelay = dT(:,2)*1000;
