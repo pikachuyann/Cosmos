0e6ef757f4a0efe082329d6d8e818a06292505d0
Nicola Paoletti <paoletti@Nicola-Pao-MacBook-Pro.local>
2015-06-18 19:14:33 +0100

    .
    Former-commit-id: 34b3a411ea9b8c4e30112b02dbb63b8e923a31fd

--- src/ECG_detect/computeECGsdistance.m
+++ src/ECG_detect/computeECGsdistance.m
@@ -1,4 +1,4 @@
-function [score,scores,ECGmean_1,ECGsd_1,ECG_distr_1,ECGmean_2,ECGsd_2,ECG_distr_2] = computeECGsdistance(ecg_1,fs_1,ecg_2,fs_2,plot,nbinsDiscreteDistr)
+function [score,scores,ECGmean_1,ECGsd_1,ECG_distr_1,ECGmean_2,ECGsd_2,ECG_distr_2] = computeECGsdistance(ecg_1,fs_1,ecg_2,fs_2,showplot,nbinsDiscreteDistr)
 
 if nargin < 6
     nbinsDiscreteDistr = 10;
@@ -8,16 +8,16 @@ if nargin < 6
 end
 
 ecgBins=min(fs_1,fs_2)/2;
-[ECGmean_1,ECGsd_1,ECG_distr_1] = computeECGDistr(ecg_1',fs_1,ecgBins,plot,'Data 1');
-[ECGmean_2,ECGsd_2,ECG_distr_2] = computeECGDistr(ecg_2',fs_2,ecgBins,plot,'Data 2');
+[ECGmean_1,ECGsd_1,ECG_distr_1] = computeECGDistr(ecg_1',fs_1,ecgBins,showplot,'Data 1');
+[ECGmean_2,ECGsd_2,ECG_distr_2] = computeECGDistr(ecg_2',fs_2,ecgBins,showplot,'Data 2');
 
 [score,scores] = discreteDistrsDistance(ECG_distr_1,ECG_distr_2,nbinsDiscreteDistr,ecgBins);
 end
 
-function [ECGmean,ECGsd,discreteDistrs] = computeECGDistr(data,fs,bins,plot,desc)
+function [ECGmean,ECGsd,discreteDistrs] = computeECGDistr(data,fs,bins,showplot,desc)
 
 if nargin == 3
-    plot = 0;
+    showplot = 0;
     desc = '';
 end
 
@@ -27,7 +27,10 @@ f = 1;                                          % approximate R-peak frequency
 bsline = LPFilter(data,.7/fs);                 % baseline wander removal (may be replaced by other approaches)
 
 x = data-bsline;
-peaks = PeakDetection(x,f/fs);                  % peak detection
+% peaks = PeakDetection(x,f/fs);                  % peak detection
+[~,peaks_idxs] = pan_tompkin(x',fs,0);
+peaks = zeros(size(x));
+peaks(peaks_idxs) = 1;
 
 [phase phasepos] = PhaseCalculation(peaks);     % phase calculation
 
@@ -35,7 +38,7 @@ pphase = PhaseShifting(phase,teta);             % phase shifting
 
 %bins = fs/2;                                     % number of phase bins
 [ECGmean,ECGsd,meanphase,discreteDistrs] = MeanECGExtraction(x,pphase,bins,1); % mean ECG extraction
-if plot
+if showplot
     t = (0:length(data)-1)/fs;
     figure;
     plot(t,data*6/max(data),'b');
