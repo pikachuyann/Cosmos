a538be42298ecf4425500979bf9051404583a817
Nicola Paoletti <paoletti@Nicola-Pao-MacBook-Pro.local>
2015-06-19 10:35:12 +0100

    .
    Former-commit-id: 63cf5dae0d9187217e71cbf90d62ee55d4de0c52 [formerly b358c368066619a10d25b5fc88bb23cefc288940]
    Former-commit-id: fa2e351399cfd9ea206867bafff3ac9b98e4142e

--- src/ECG_detect/initHeartModel.m
+++ src/ECG_detect/initHeartModel.m
@@ -3,15 +3,19 @@ load workspace_unstruct.mat
 % tLimit = 120; %2 minutes
 % tLimit_sim = 120;
 % [ecg_sig,fs] = extractSignal(idRecording, tLimit);
-% ecg_sig=EKG_andrea_light_activity(1:ceil(end/2));
-ecg_sig=EKG_andrea_light_activity;
+% ecg_sig=EKG_andrea_light_activity;
+ecg_sig=EKG_chris(1:end/4);
 fs=100;
-tLimit_sim = length(ecg_sig)/fs;
+% ecg_sig=EKG3;
+% fs=360;
+tLimit_sim = min(length(ecg_sig)/fs,120);
 [ParECG,filteredECG] = genECGParam(ecg_sig,fs,1,true);
 out=sim('HeartModelECG2','StopTime', sprintf('%d',tLimit_sim),'SaveOutput','on','OutputSaveName','heart_Events');
 synth_ECG=ECGfromTrace(get(out,'heart_Events'),ParECG,1/fs);
 score=computeECGsdistance(filteredECG,fs,synth_ECG,fs,true);
 
+
+mean(ParECG)
 %out=sim('HeartModelECG','StopTime', sprintf('%d',tLimit_sim),'SaveOutput','on','OutputSaveName','synth_ECG');
 %synth_ECG=get(out,'synth_ECG');
 %score=computeECGsdistance(filteredECG,fs,synth_ECG,fs,true);
