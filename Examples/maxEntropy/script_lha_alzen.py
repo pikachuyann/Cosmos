152a347e76e1e02bdd6790660efaa0664df54256
Nicolas Basset <basset@bassetlaptop.cs>
2015-08-07 16:23:01 +0100

    voila
    Former-commit-id: e921ca36b68ca10c23e78d97c88606b64a873751

--- Examples/maxEntropy/script_lha_alzen.py
+++ Examples/maxEntropy/script_lha_alzen.py
@@ -1,38 +1,35 @@
 #!env python
 
-n=3
+n=5
 s="";
 s+="NOTDETERMINISTIC\n\n";
-s+="const double Dline=500;\n";
-s+="const double wt=100;\n";
+s+="const double Dline=1000;\n";
+s+="const double wt=1000;\n";
 s+="const double B = 20;\n";
-s+="const double C = 19;\n\n";
-s+="const double D = 30;\n\n";
-s+="VariablesList = {time,DISC countT, x};\n"
+s+="const double C = 10;\n\n";
+s+="const double D = 9;\n\n";
+s+="const double E = 50;\n\n";
+s+="const double F = 100;\n\n";
+s+="VariablesList = {time,DISC countT, x,t};\n"
 states=["l_%d" %i for i in range(n)];
 statesfin=["lf_%d" %i for i in range(n)];
-statesbis=["ll_%d" %i for i in range(1,n)];
+statesbis=["ll_%d" %i for i in range(n)];
 s+="LocationsList = {"
 for i in range(n):
-    s+=states[i]+","+statesfin[i]+",";
-for i in range(n-1):
-    s+=statesbis[i]+",";
+    s+=states[i]+","+statesfin[i]+","+statesbis[i]+",";
 s+="lff};\n";
 s+="PROB;\n\n"
 s+="InitialLocations={l_0};\nFinalLocations={lff};\n";
 s+="Locations={\n"
 for i in range(n):
-    s+="(l_%d, TRUE, (time:1,x:1));" %i+"\n(lf_%d, TRUE, (time:1,x:1));\n"%i;
-for i in range(1,n):
-    s+="(ll_%d, TRUE, (time:1,x:1));\n"%i;
-s+="(lff, TRUE, (time:1,x:1));\n};\n\n"
+    s+="(l_%d, TRUE, (time:1,x:1,t:1));" %i+"\n(lf_%d, TRUE, (time:1,x:1,t:1));\n"%i;
+    s+="(ll_%d, TRUE, (time:1,x:1,t:1));\n"%i;
+s+="(lff, TRUE, (time:1,x:1,t:1));\n};\n\n"
 s+="Edges={\n"
 
 for i in range(n):
     s+="((l_%d" %i + ",l_%d"%i+"),ALL\{t_a,t_b,t_c,t_d,t_e},countT <=Dline ,#);\n";    
-for i in range(1,n):
     s+="((ll_%d" %i + ",ll_%d"%i+"),ALL\{t_a,t_b,t_c,t_d,t_e},countT <=Dline ,#);\n";    
-for i in range(n):
     s+="((lf_%d" %i + ",lf_%d"%i+"),ALL\{t_a,t_b,t_c,t_d,t_e},countT <=Dline ,#);\n";    
 for i in range(n):
     s+="((lf_%d" %i + ",lff),ALL,countT =Dline+1,#);\n";
@@ -50,11 +47,15 @@ for i in range(n-1):
 
 
 s+="\n%pattern to be recognised (leading to the final states)\n"
-s+="((l_0,ll_1),{t_a}, x<=B  ,{countT = countT+1});\n";
+s+="((l_0,ll_1),{t_a}, x<=D  ,{countT = countT+1,t=0});\n";
+s+="((ll_0,ll_1),{t_a}, x<=D  ,{countT = countT+1});\n";
 for i in range(1,n-2):
-    s+="((ll_%d" %i + ",ll_%d"%(i+1)+"),{t_a}, x<=C  ,{countT = countT+1});\n";
-s+="((ll_%d"%(n-2)+",ll_%d"%(n-1) +"),{t_b}, x<=C  ,{countT = countT+1});\n";
-s+="((ll_%d"%(n-1)+",lf_0),{t_e}, x<=wt,  {countT = countT+1,x'=0});\n";
+    s+="((ll_%d" %i + ",ll_%d"%(i+1)+"),{t_a}, x<=D  ,{countT = countT+1});\n";
+s+="((ll_%d"%(n-2)+",ll_%d"%(n-1) +"),{t_b}, x<=D & t<=E ,{countT = countT+1});\n";
+s+="((ll_%d"%(n-1)+",lf_0),{t_e}, x<=wt& t>=F,  {countT = countT+1,x'=0});\n";
+for i in range(n-1):
+    s+="((ll_%d" %i + ",ll_0),{t_d}, x<=B  ,{countT = countT+1});\n";
+
 
 s+="%copy of the automaton when the final states are reached\n";
 s+="%forward edges in the automaton\n"
