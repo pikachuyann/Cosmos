f63298402028db32bd3946df72c75212bf147e00
Nicola Paoletti <paoletti@Nicola-Pao-MacBook-Pro.local>
2015-08-13 17:33:34 +0100

    .
    Former-commit-id: 90d5204e6a48e7163bee3427120dfc9cb62145d7 [formerly f9ae77531c6d0704c0184df45ca4aa7d012ec3e9]
    Former-commit-id: 37593128b4d0e611e1ca43f10f866648e28857fc

--- src/syn/battery.py
+++ src/syn/battery.py
@@ -1,3 +1,141 @@
+import socket
+
+def GetReward(cname, modelname, propName):
+    cmd=cname
+    cmd += " "+modelname
+    cmd += " "+propName
+    #cmd += format(" --const \"SA_d=%f\""%x)
+    cmd += " --batch 100 --max-run 5000 --njob 2"
+    print(cmd+"\n")
+    os.system(cmd)
+    os.system("grep \"Estimated value\" Result_waitDepleted.res | sed \"s/Estimated value:\t//g\" > tmpResult")
+    v = eval(get_my_string("tmpResult"))
+    return v
+
+def GetSumCurrent(pmData, monitorSamplingFreq):
+	sumCurrent = 0
+	prev = pmData[0][1]
+
+	it_beg = 0
+
+	for it in range(1,len(pmData)):
+		if pmData[it][1]!=2 and prev==2:
+			it_beg = it
+			break
+
+	cnt = 0		
+	for it in range(it_beg,len(pmData)):
+		if pmData[it][1]==2 and cnt>=20:
+			break
+		elif pmData[it][1]==2 and cnt<20: 	
+			cnt = cnt + 1
+		elif prev==2 and pmData[it][1]!=2:
+			cnt = 0
+		sumCurrent = sumCurrent + pmData[it][0]
+		prev = pmData[it][1]
+
+	
+	return sumCurrent
+
+def GetTotalCurrents(handle, esamples, monitorSamplingFreq):
+		cummCurrentList = []
+
+		# Send get list of IDs
+		handle.sendall('\xF3')
+
+		bufSize = bytearray(4)
+		buflen = handle.recv_into(bufSize, 4)
+
+		if buflen!=4:
+			print "Wrong packet length"
+			return cummCurrentList
+
+		bufSize = struct.unpack("I",bufSize)
+
+		print "Packet size: "+str(bufSize[0])
+
+		bufIDs = bytearray()
+		buftmp = bytearray(2048)
+		bytes_recd = 0
+
+		while bytes_recd < bufSize[0]:
+			chunkLen = handle.recv_into(buftmp,2048)
+			bufIDs[bytes_recd:] = buftmp[:chunkLen]
+			bytes_recd = bytes_recd + chunkLen
+
+		if 	bytes_recd != bufSize[0]:
+			print "Wrong data"
+			return cummCurrentList
+
+		nTranCnt = 	bufSize[0]/8
+		print "Number of transitions received: "+str(nTranCnt)
+
+		tTimeList = []
+		for idx in range(0,nTranCnt):
+			tID = bufIDs[idx*8]
+			tTime = struct.unpack("I",bufIDs[idx*8+1:idx*8+1+4])
+			tTimeList.append([tID, tTime[0]])
+			#print str(tTime[0])+" "+str(bufIDs[idx*8])
+
+		toRemove = []	
+		for idx in range(len(tTimeList)-1):
+			if tTimeList[idx][1]==tTimeList[idx+1][1]:
+				toRemove.append(tTimeList[idx])
+
+		for rem in toRemove:
+			tTimeList.remove(rem)
+
+		minCurrent = GetMinCurrent(esamples)
+		cummCurrentList = GetEnergyReadings(esamples, monitorSamplingFreq, tTimeList, minCurrent)
+
+		return cummCurrentList
+
+def GetEnergyReadings(pmData, monitorSamplingFreq, tranListTimes, minCurrent):
+	cummCurrentList = []
+
+	sumCurrent = 0
+	sampleCnt = 1
+	prev = pmData[0][1]
+	
+	timeList = [item[1] for item in tranListTimes]
+	cummCurrentList = [[item[0],item[1],0] for item in tranListTimes]
+
+	it_beg = 0
+
+	for it in range(1,len(pmData)):
+		if pmData[it][1]!=2 and prev==2:
+			it_beg = it
+			break
+
+	tmpSampleCnt = 0
+	timeListIdx = 0
+
+	for it in range(it_beg,len(pmData)):
+		stime = sampleCnt/monitorSamplingFreq
+		sumCurrent = sumCurrent + pmData[it][0]
+		tmpSampleCnt = tmpSampleCnt + 1
+
+		if timeListIdx>=len(timeList):
+			break
+
+		if stime>=timeList[timeListIdx]:
+			cummCurrentList[timeListIdx][2] = sumCurrent - tmpSampleCnt * minCurrent
+			timeListIdx = timeListIdx + 1
+			sumCurrent = 0
+			tmpSampleCnt = 0
+
+		sampleCnt = sampleCnt + 1
+
+	
+	filedta = open('markers.txt', 'w+')
+
+	for it in range(it_beg,len(pmData)):
+		SaveToFileAll(filedta, pmData[it][1], pmData[it][0])
+
+	filedta.close()
+	return cummCurrentList
+
+
 def SaveBatteryParams(handle, pardict):
 
 	for key in pardict.keys():
@@ -60,13 +198,19 @@ def SaveBatteryDistribution(handle, dData):
 	
 
 	handle.write("\n")	
-	handle.write("RetValTR RetStruct(int id)\n")
-	handle.write("{\n")
+	# handle.write("RetValTR RetStruct(int id)\n")
+	# handle.write("{\n")
+	# handle.write("\tRetValTR ret = {NULL,0};\n")
+
+	# handle.write("\n")
+	# handle.write("\tswitch(id) {\n")
+
+	handle.write("double magicUDDD(vector<double> const& param,unsigned int rand){\n")
 	handle.write("\tRetValTR ret = {NULL,0};\n")
-
+	handle.write("\tdouble timeVal = 0.0;\n")
 	handle.write("\n")
+	handle.write("\tswitch((int)param[0]) {\n")
 
-	handle.write("\tswitch(id) {\n")
 	for key in dIdCurr:
 		handle.write("\t\tcase "+str(key)+":\n")
 		handle.write("\t\t\tret.pt = & tr"+str(key)+"[0];\n")
@@ -74,9 +218,14 @@ def SaveBatteryDistribution(handle, dData):
 		handle.write("\t\t\tbreak;\n")
 
 	handle.write("\t}\n")
+	handle.write("\ttimeVal = ret.pt[idx].time;\n")
+	handle.write("\tgCurrentReward = ret.pt[idx].reward/timeVal;\n")
 	handle.write("\n")
-	handle.write("\treturn ret;\n")
+	handle.write("\treturn timeVal;\n")
 	handle.write("}\n")
+	# handle.write("\n")
+	# handle.write("\treturn ret;\n")
+	# handle.write("}\n")
 
 
 	SaveBatteryFooter(handle)	
@@ -84,31 +233,6 @@ def SaveBatteryDistribution(handle, dData):
 
 
 def SaveBatteryFooter(handle):
-	handle.write("\n");
-
-	handle.write("double TransitionTime(int id)\n")
-	handle.write("{\n")
-	handle.write("\tdouble timeVal = 0.0;\n");
-	handle.write("\tRetValTR rt = RetStruct(id);\n")
-	
-	handle.write("\n");
-
-	handle.write("\tif (!rt.size)\n")
-	handle.write("\t\t return gAvgReward;\n")
-	handle.write("\n");
-
-	handle.write("\tint idx = rand() % rt.size;\n")
-	
-	handle.write("\n");
-
-	handle.write("\ttimeVal = rt.pt[idx].time;\n")
-	handle.write("\tgCurrentReward = rt.pt[idx].reward/timeVal;\n")
-	
-	handle.write("\n");
-
-	handle.write("\treturn timeVal;\n")
-	handle.write("}\n")
-
 	handle.write("\n");
 
 	handle.write("double y1fun(double c, double kp, double k, double t, double y0, double y10, double y20, double I)\n")
