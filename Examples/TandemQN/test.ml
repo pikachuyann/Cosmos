30c1751a1d45a3b11a431e69984b03e33f30f12d
Benoît Barbot <benoit.barbot@u-pec.fr>
2015-09-23 17:52:01 +0200

    prog
    Former-commit-id: b88248a673d845ab294069959da490240d92927e

--- Examples/TandemQN/test.ml
+++ Examples/TandemQN/test.ml
@@ -1,3 +1,3 @@
 #use "../testTeamCity.ml";;
 
-test_cosmos_gspn "tqn" ["Overflow",0.1752] "--max-run 5000"
+test_cosmos_gspn "tqn" ["Overflow",0.1752; "maxVal",5.401088487352618] "--max-run 5000  --HASL-formula 'maxVal=AVG(Max(x1));'" 
