4c344b8fd362c3880a6a29ff482d1aea223c2dcc
Benoît Barbot <benoit.barbot@u-pec.fr>
2015-12-02 14:02:25 +0100

    Revert "Revert "First working version""
    This reverts commit 1eb9445607a1df90639f6f2d14f552810ef69068 [formerly 8220cfc6912e3786c2447d1732c4328bee727d4e].
    
    
    Former-commit-id: 7383b2a8725f1b1102bb43bad84d89a1517242ce

--- src/Simulator/spn_orig.cpp
+++ src/Simulator/spn_orig.cpp
@@ -203,7 +203,6 @@ void SPN_orig::update(double ctime,size_t E1_transitionNum, const abstractBindin
     }
     //assert(cerr<< "assert!"<< endl);
     
-    
     /*
      //In Debug mode check that transition are scheduled iff they are enabled
      for (const auto &t : Transition){
