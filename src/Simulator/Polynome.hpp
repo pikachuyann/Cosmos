159876d7b497725b3c15357d0f609cc4ff5c89f2
Benoît Barbot <benoit.barbot@u-pec.fr>
2016-02-11 11:38:07 +0100

    add statflow model
    Former-commit-id: f86a992c4cbe88ea000902afd8df1f742d09a0d6

---
+++ src/Simulator/Polynome.hpp
@@ -0,0 +1,17 @@
+//
+//  Polynome.h
+//  Cosmos
+//
+//  Created by Benoît Barbot on 07/01/2016.
+//
+//
+
+#ifndef Polynome_h
+#define Polynome_h
+
+template <unsigned int N>
+struct Monome<N> {
+    
+};
+
+#endif /* Polynome_h */
