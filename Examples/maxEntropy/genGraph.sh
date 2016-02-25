ce22ecb2a0b5e116a3ac2a6d11672ee282378ab7
Benoît Barbot <benoit.barbot@u-pec.fr>
2016-02-25 14:26:05 +0100

    pre not constexpr
    Former-commit-id: 56373eebc995071fda797910be233c5b94b32873 [formerly 27c194049d8f0558a9a6fd7ee3aacda068f46892]
    Former-commit-id: 5a3583900016a58b9fc6cae21a715b2da23be242

--- Examples/maxEntropy/genGraph.sh
+++ Examples/maxEntropy/genGraph.sh
@@ -1,9 +1,9 @@
 
 
 
-sage script_tocosmos.sage $1 $1.grml 1 -unif fldsjflds
+sage script_tocosmos.sage $1 $1.grml 10 -unif fldsjflds
 
-Cosmos $1.grml --loop "#400000" --trace-place "x_1,x_2,x_3" --output-trace $1.dat 0.0 --max-run 1 --gppflags "-O0" 
+Cosmos $1.grml --loop "#400000" --trace-place "x_1,x_2,x_3" --output-trace $1.dat 0.0 --max-run 1
 grep 'tt_' $1.dat > preplot.dat
 grep 'tt_0_0\|tt_1_0\|tt_2_0' $1.dat > preplota.dat
 grep 'tt_0_1\|tt_1_1\|tt_2_1\|tt_3_0' $1.dat > preplotb.dat
@@ -13,7 +13,7 @@ gnuplot gnplotscript.gp
 mv gnuresult.png $1_1.png
 
 #exit 0
-Cosmos Iso_$1.grml --loop "#400000" --trace-place "x_1,x_2,x_3" --output-trace Iso_$1.dat 0.0 --max-run 1 --gppflags "-O0"
+Cosmos Iso_$1.grml --loop "#400000" --trace-place "x_1,x_2,x_3" --output-trace Iso_$1.dat 0.0 --max-run 1
 grep 'tt_' Iso_$1.dat > preplot.dat
 grep 'tt_0_0\|tt_1_0\|tt_2_0' Iso_$1.dat > preplota.dat
 grep 'tt_0_1\|tt_1_1\|tt_2_1\|tt_3_0' Iso_$1.dat > preplotb.dat
