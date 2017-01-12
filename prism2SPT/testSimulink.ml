d3114c652b5a95d890433f2a4db69949edc28839
Yann Duplouy <yann@duplouy.name>
2017-01-12 11:39:27 +0100

    Génération de code TikZ représentant les modèles Simulink
    Former-commit-id: 0e19c8d18556e82c78de45291b4503ea9e530679

--- prism2SPT/testSimulink.ml
+++ prism2SPT/testSimulink.ml
@@ -68,6 +68,6 @@ let _ =
 	|> Xml.parse_string
 	|> Simulinkparser.blocklist_of_tree ([],[],[])
         |> Simulinkparser.pushDefaults
-	|> Simulinkparser.printPModel stdout
+	|> Simulinkparser.printLaTeX stdout
 
 
