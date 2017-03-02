6ca1b1de0037e38f220a0294c3874c91d5344617
Benoît Barbot <benoit.barbot@u-pec.fr>
2017-03-02 12:18:07 +0100

    prog
    Former-commit-id: 37b13a5e94f3c58b1f2d0198b0c123c20b9ae81c [formerly 4829bc0b83df94098a66a563309ced4ea4972729]
    Former-commit-id: 2268f8cf6a99610073bd2dff741052c1b9bb67a3

--- Examples/TestEventsQueueSet/test.ml
+++ Examples/TestEventsQueueSet/test.ml
@@ -1,8 +1,7 @@
 #use "../testTeamCity.ml";;
 
 
-Cosmos -g moded_CoSimulation.grml propCollision.lha --width 0.01 --use-setdomain-impl --njob 8 --batch 20 --max-run 10000000 --const EnvBehaviour=1 --const TypeGeneration=2 --const Ncases=1000 --const Vmax=5 --const VminInit=3 --const Ncases=1000 --const Nveh=20
 
-test_cosmos "TestEventQueueSet" "moded_CoSimulation.grml" "propCollision.lha" "--max-run 20000 --use-setdomain-impl --batch 0 --const EnvBehaviour=1 --const TypeGeneration=2 --const Ncases=1000 --const Vm\
-ax=5 --const VminInit=3 --const Ncases=1000 --const Nveh=20"
+test_cosmos "TestEventQueueSet" "moded_CoSimulation.grml" "propCollision.lha" 
+  "--max-run 20000 --use-setdomain-impl --batch 0 --const EnvBehaviour=1 --const TypeGeneration=2 --const Ncases=1000 --const Vmax=5 --const VminInit=3 --const Ncases=1000 --const Nveh=20"
   []
