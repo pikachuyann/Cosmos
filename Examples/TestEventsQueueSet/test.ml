a15c8f0ef6ebc62af2ff49b1bd1bccb9c7f45952
Benoît Barbot <benoit.barbot@u-pec.fr>
2017-03-02 14:28:23 +0100

    add test
    Former-commit-id: d03721ec6961403f062eb9f914dcaaa57222ec8a [formerly e66c13c6f5093b019c07b400a1434742c3be3363]
    Former-commit-id: 3703fae29eee8dc100930c54b929491641e1fcb5

--- Examples/TestEventsQueueSet/test.ml
+++ Examples/TestEventsQueueSet/test.ml
@@ -4,4 +4,4 @@
 
 test_cosmos "TestEventQueueSet" "moded_CoSimulation.grml" "propCollision.lha" 
   "--max-run 20000 --use-setdomain-impl --batch 0 --const EnvBehaviour=1 --const TypeGeneration=2 --const Ncases=1000 --const Vmax=5 --const VminInit=3 --const Ncases=1000 --const Nveh=20"
-  []
+  [collb,0.814241486068111]
