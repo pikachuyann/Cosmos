e77cced5e74ffd2c26dc830a64431e9c589acf1b
Yann Duplouy <yann@duplouy.name>
2017-03-28 13:59:24 +0200

    Instanciation d'un type particulier pour les temps (pour tenter d'avoir une meilleure précision)
    Former-commit-id: 4e7130ee0ab117f45a21817e5ea855559ce86dd6

---
+++ modelConvert/SKModelCommons.hpp
@@ -0,0 +1,32 @@
+#ifndef SKModel_Commons
+#define SKModel_Commons
+
+class SKTime {
+public:
+	SKTime();
+	SKTime(double);
+	SKTime(size_t,int64_t);
+
+	void updatePrecision(size_t);
+	void set_to(double);
+	void increment(double);
+	size_t getPrecision();
+	int64_t getBareTime();
+	double getDouble();
+
+	SKTime& operator = (double);
+	SKTime operator + (SKTime);
+	SKTime operator - (SKTime);
+	SKTime operator + (double);
+	SKTime operator - (double);
+	bool operator < (SKTime);
+	bool operator > (SKTime);
+//	operator double();
+
+private:
+	size_t precision;
+	int64_t time;
+	double power;
+};
+
+#endif
