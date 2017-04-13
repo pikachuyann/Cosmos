90ae457417d92ce0ec536814779c2c1e97082cc8
Benoît Barbot <benoit.barbot@u-pec.fr>
2017-04-13 10:15:22 +0200

    progress
    Former-commit-id: c6cc3b32eea521177cdd8efc4c1f884d0c6c6dec

--- modelConvert/build.sh
+++ modelConvert/build.sh
@@ -4,9 +4,9 @@ mkdir -p ../bin
 rm -rf tmp
 #ocamlbuild -use-ocamlfind -j 4 -pp "camlp4o pa_macro.cmo -DHAS_XML -DHAS_ZIP" -I ClessParser prism2SPT.native modelConvert.native parserUnparseAttribute.native
 ocamlbuild -use-ocamlfind -j 4 -I ClessParser prism2SPT.native StochasticPetriNet.cmo modelConvert.native parserUnparseAttribute.native modelConvert.byte testSimulink.native
-cd _build
-ocamlmktop type.cmo prismType.cmo parserPrism.cmo lexerPrism.cmo PetriNet.cmo StochPTPrinter.cmo SemanticPT.cmo StochasticPetriNet.cmo -o petriscript
-cd ..
+#cd _build
+#ocamlmktop type.cmo prismType.cmo parserPrism.cmo lexerPrism.cmo PetriNet.cmo StochPTPrinter.cmo SemanticPT.cmo StochasticPetriNet.cmo -o petriscript
+#cd ..
 
 cp modelConvert.native ../bin/modelConvert
 cp testSimulink.native ../bin/testSimulink
