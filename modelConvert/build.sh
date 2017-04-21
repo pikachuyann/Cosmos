dcb41769981efd17aea43cd67b853396bdc33b6f
Benoît Barbot <benoit.barbot@u-pec.fr>
2017-04-21 16:20:31 +0200

    move prism files
    Former-commit-id: d1e80cf9e539e68ed0216680db92cff8b4093712

--- modelConvert/build.sh
+++ modelConvert/build.sh
@@ -3,7 +3,7 @@
 mkdir -p ../bin
 rm -rf tmp
 #ocamlbuild -use-ocamlfind -j 4 -pp "camlp4o pa_macro.cmo -DHAS_XML -DHAS_ZIP" -I ClessParser prism2SPT.native modelConvert.native parserUnparseAttribute.native
-ocamlbuild -use-ocamlfind -j 4 -I ClessParser prism2SPT.native StochasticPetriNet.cmo modelConvert.native parserUnparseAttribute.native modelConvert.byte testSimulink.native
+ocamlbuild -use-ocamlfind -j 4 -I ClessParser -I Prism prism2SPT.native StochasticPetriNet.cmo modelConvert.native parserUnparseAttribute.native modelConvert.byte testSimulink.native
 #cd _build
 #ocamlmktop type.cmo prismType.cmo parserPrism.cmo lexerPrism.cmo PetriNet.cmo StochPTPrinter.cmo SemanticPT.cmo StochasticPetriNet.cmo -o petriscript
 #cd ..
